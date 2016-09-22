e0749811769bebbfa83a605d3ecb31f6e07eccff
Glen Maddern <glenmaddern@gmail.com>
2016-09-23 09:35:51 +1000

    fixed toggle test

--- src/constructors/test/toggle.test.js
+++ src/constructors/test/toggle.test.js
@@ -2,10 +2,6 @@ import expect from 'expect'
 import toggle from '../toggle'
 
 describe('toggle', () => {
-  const defaultRule = 'default: rule'
-  const optionA = 'option: a'
-  const optionB = 'option: b'
-
   it('should handle an empty case', () => {
     const namespace = toggle('namespace', {})
     expect(namespace()).toEqual([])
@@ -15,49 +11,49 @@ describe('toggle', () => {
 
   it('should always inject default even if no input passed', () => {
     const namespace = toggle('namespace', {
-      default: defaultRule,
+      default: 'default: rule',
     })
-    expect(namespace()).toEqual([defaultRule])
-    expect(namespace('')).toEqual([defaultRule])
-    expect(namespace(' ')).toEqual([defaultRule])
+    expect(namespace()).toEqual(['default: rule;'])
+    expect(namespace('')).toEqual(['default: rule;'])
+    expect(namespace(' ')).toEqual(['default: rule;'])
   })
 
   it('should handle any combination of inputs', () => {
     const namespace = toggle('namespace', {
-      default: defaultRule,
-      a: optionA,
-      b: optionB,
+      default: 'default: rule',
+      a: 'option: a',
+      b: 'option: b',
     })
-    expect(namespace()).toEqual([defaultRule])
-    expect(namespace('a')).toEqual([defaultRule, optionA])
-    expect(namespace('a b')).toEqual([defaultRule, optionA, optionB])
-    expect(namespace('b a')).toEqual([defaultRule, optionB, optionA])
-    expect(namespace('b')).toEqual([defaultRule, optionB])
+    expect(namespace()).toEqual(['default: rule;'])
+    expect(namespace('a')).toEqual(['default: rule;', 'option: a;'])
+    expect(namespace('a b')).toEqual(['default: rule;', 'option: a;', 'option: b;'])
+    expect(namespace('b a')).toEqual(['default: rule;', 'option: b;', 'option: a;'])
+    expect(namespace('b')).toEqual(['default: rule;', 'option: b;'])
   })
 
   it('should be ok without a default', () => {
     const namespace = toggle('namespace', {
-      a: optionA,
-      b: optionB,
+      a: 'option: a',
+      b: 'option: b',
     })
     expect(namespace()).toEqual([])
-    expect(namespace('a')).toEqual([optionA])
-    expect(namespace('a b')).toEqual([optionA, optionB])
-    expect(namespace('b a')).toEqual([optionB, optionA])
-    expect(namespace('b')).toEqual([optionB])
+    expect(namespace('a')).toEqual(['option: a;'])
+    expect(namespace('a b')).toEqual(['option: a;', 'option: b;'])
+    expect(namespace('b a')).toEqual(['option: b;', 'option: a;'])
+    expect(namespace('b')).toEqual(['option: b;'])
   })
 
   it('should throw if passed something not present', () => {
     const namespace = toggle('namespace', {
-      a: optionA,
+      a: 'option: a',
     })
     expect(() => namespace('b')).toThrow("namespace: Unknown value 'b'. Valid values are:\na")
   })
 
   it('should not mention the default as a valid', () => {
     const namespace = toggle('namespace', {
-      default: defaultRule,
-      a: optionA,
+      default: 'default: rule',
+      a: 'option: a',
     })
     expect(() => namespace('b')).toThrow("namespace: Unknown value 'b'. Valid values are:\na")
   })
