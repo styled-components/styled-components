f9aeb7918dadac7fee9d8da9059ab5692d792b90
Glen Maddern <glenmaddern@gmail.com>
2016-09-22 12:52:03 +1000

    ugh flow can gtfkd right now

--- src/constructors/css2.js
+++ src/constructors/css2.js
@@ -1,7 +1,5 @@
 // @flow
 import hyphenate from 'fbjs/lib/hyphenateStyleName'
-import isFunction from 'lodash/isFunction'
-import isPlainObject from 'lodash/isPlainObject'
 
 export type Interpolation = Function | Object | string
 
@@ -11,20 +9,20 @@ export const interleave = (strings: Array<string>, interpolations: Array<Interpo
   ), [strings[0]])
 )
 
-type StaticAndDynamic = Array<String | Function>
+type StaticAndDynamic = Array<string | Function>
 
 export const objToCss = (obj: Object): string => (
   Object.keys(obj).map(k => `${hyphenate(k)}: ${obj[k]};`).join(" ")
 )
 
 export const flatten = (chunks: Array<Interpolation>) : StaticAndDynamic => (
-  chunks.reduce((array, chunk) => {
+  chunks.reduce((array, chunk: Interpolation) => {
     /* Remove falsey values */
     if (!chunk) return array
     /* Defer functions */
-    if (isFunction(chunk)) return array.concat(chunk)
+    if (typeof chunk === 'function') return array.concat(chunk)
     /* Handle objects */
-    const stringChunk = isPlainObject(chunk) ? objToCss(chunk) : chunk.toString()
+    const stringChunk: string = typeof chunk === 'object' ? objToCss(chunk) : chunk.toString()
     const last = array[array.length - 1]
     if (last && typeof last === "string") {
       return array.slice(0, -1).concat(`${last}${stringChunk}`)
