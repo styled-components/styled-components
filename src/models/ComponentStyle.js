db1bd97b35c51252bfa083e6bad89d3ada0b4285
Glen Maddern <glenmaddern@gmail.com>
2016-09-23 06:17:27 +1000

    its amazingly easy when you put it like this

--- src/models/ComponentStyle.js
+++ src/models/ComponentStyle.js
@@ -1,56 +1,45 @@
 // @flow
 import parse from '../postcss/parse'
 import postcssNested from '../postcss/postcss-nested'
-import { hashObject } from 'aphrodite/lib/util'
-import {StyleSheet }from 'glamor/lib/sheet'
+import {hashObject} from 'aphrodite/lib/util'
+import {StyleSheet}from 'glamor/lib/sheet'
 const styleSheet = new StyleSheet()
 styleSheet.inject()
+const inserted = {}
 
 import type {RuleSet} from "../types"
 import flatten from "../utils/flatten"
 
 /*
-  ComponentStyle is all the CSS-specific stuff, not
-  the React-specific stuff.
+ ComponentStyle is all the CSS-specific stuff, not
+ the React-specific stuff.
  */
 export default class ComponentStyle {
   rules: RuleSet;
+
   constructor(rules: RuleSet) {
     this.rules = rules
   }
 
   /*
    * Flattens a rule set into valid CSS
-   * Parses the CSS
-   * Figures out how to inject it
+   * Hashes it, wraps the whole chunk in a ._hashName {}
+   * Parses that with PostCSS then runs PostCSS-Nested on it
+   * Injects that using Glamor's StyleSheet impl.
    * */
   injectStyles(executionContext: Array<any>) {
     const flatCSS = flatten(this.rules, executionContext).join("")
     console.log(flatCSS)
     const hash = '_' + hashObject(flatCSS)
-    console.log(hash)
-    const root = parse(`.${hash} { ${ flatCSS } }`);
-    console.log(root)
-    postcssNested(root)
-    const result = root.toResult().css;
-    console.log(result)
-    styleSheet.insert(result)
+    if (!inserted[hash]) {
+      const root = parse(`.${hash} { ${ flatCSS } }`);
+      console.log(root)
+      postcssNested(root)
+      const result = root.toResult().css;
+      console.log(result)
+      inserted[hash] = true
+      styleSheet.insert(result)
+    }
     return hash
-
-    /* Thoughts. I don't need to follow the existing implementation with
-    * rules and fragments because i can start injecting styles directly.
-    * We can simply hash the entire string as it comes through at this point.
-    * Then... maybe we use postcss nested on it and wrap it all in .hash {} */
-    // const rules = {}
-    // const fragments = []
-    // root.each(node => {
-    //   if (node.type === 'decl') {
-    //     rules[camelizeStyleName(node.prop)] = node.value;
-    //   } else if (node.type === 'rule') {
-    //
-    //   }
-    // })
-    // console.log(rules)
-    // console.log(fragments)
   }
 }
