61b3212b25d4627067c9bd6b6869850134ad94b6
Glen Maddern <glenmaddern@gmail.com>
2016-09-22 12:29:18 +1000

    flatten is handling simple series of objects

--- src/constructors/css2.js
+++ src/constructors/css2.js
@@ -1,10 +1,37 @@
 // @flow
+import hyphenate from 'fbjs/lib/hyphenateStyleName'
+import isFunction from 'lodash/isFunction'
+import isPlainObject from 'lodash/isPlainObject'
+
 export type Interpolation = Function | Object | string
 
 export const interleave = (strings: Array<string>, interpolations: Array<Interpolation>) => (
   interpolations.reduce((array, interp, i) => (
-    array.concat(interp, strings[i+1])
+    array.concat(interp, strings[i + 1])
   ), [strings[0]])
+)
+
+type StaticAndDynamic = Array<String | Function>
+
+export const objToCss = (obj: Object): string => (
+  Object.keys(obj).map(k => `${hyphenate(k)}: ${obj[k]};`).join(" ")
+)
+
+export const flatten = (chunks: Array<Interpolation>) : StaticAndDynamic => (
+  chunks.reduce((array, chunk) => {
+    /* Remove falsey values */
+    if (!chunk) return array
+    /* Defer functions */
+    if (isFunction(chunk)) return array.concat(chunk)
+    /* Handle objects */
+    const stringChunk = isPlainObject(chunk) ? objToCss(chunk) : chunk.toString()
+    const last = array[array.length - 1]
+    if (last && typeof last === "string") {
+      return array.slice(0, -1).concat(`${last}${stringChunk}`)
+    } else {
+      return array.concat(stringChunk)
+    }
+  }, [])
 )
 
 export default (strings: Array<string>, ...interpolations: Array<Interpolation>) => {
--- src/constructors/test/css2.test.js
+++ src/constructors/test/css2.test.js
@@ -1,5 +1,5 @@
 import expect from 'expect'
-import css, {interleave} from '../css2'
+import css, {interleave, flatten} from '../css2'
 
 describe('interleave', () => {
   it('blindly interleave', () => {
@@ -13,5 +13,23 @@ describe('interleave', () => {
     expect(interleave(['foo', 'bar', 'baz'], [1])).toEqual(['foo', 1, 'bar'])
     expect(interleave([], [1])).toEqual([undefined, 1, undefined])
     expect(interleave(['foo'], [1, 2, 3])).toEqual(['foo', 1, undefined, 2, undefined, 3, undefined])
+  })
+})
+
+describe('flatten', () => {
+  it('merges strings', () => {
+    expect(flatten(['foo', 'bar', 'baz'])).toEqual(['foobarbaz'])
+  })
+  it('drops nulls', () => {
+    expect(flatten(['foo', false, 'bar', undefined, 'baz', null])).toEqual(['foobarbaz'])
+  })
+  it('hypenates objects', () => {
+    const obj = {
+      fontSize: '14px',
+      WebkitFilter: 'blur(2px)'
+    }
+    const css = 'font-size: 14px; -webkit-filter: blur(2px);'
+    expect(flatten([obj])).toEqual([css])
+    expect(flatten(['some:thing;', obj, 'something: else;'])).toEqual([`some:thing;${css}something: else;`])
   })
 })
