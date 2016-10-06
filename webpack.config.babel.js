3ea63cdc62e9e0f1a19b32fefeec920dd8ed0625
Max <contact@mstoiber.com>
2016-10-06 10:06:46 +0200

    Build both UMD versions in same build
    There's no need to run webpack twice.

---
+++ webpack.config.babel.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = [{
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'styled-components.js',
+    library: 'styled-components',
+    libraryTarget: 'umd',
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        loader: 'json',
+        test: /\.json$/,
+      },
+    ],
+  },
+  plugins: [new webpack.DefinePlugin({ 'process.env.NODE_ENV': 'production' })],
+}, {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'styled-components.min.js',
+    library: 'styled-components',
+    libraryTarget: 'umd',
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        loader: 'json',
+        test: /\.json$/,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': 'production' }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+  ],
+}]
