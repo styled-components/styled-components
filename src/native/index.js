5821328c0eac1ea70f5f86b75fa12afde6a648e7
Glen Maddern <glenmaddern@gmail.com>
2016-10-10 16:15:20 +1100

    changing the way the aliases are attached

--- src/native/index.js
+++ src/native/index.js
@@ -7,55 +7,27 @@ import type { Interpolation, Target } from '../types'
 
 const styled = (tag: Target) =>
   (strings: Array<string>, ...interpolations: Array<Interpolation>) =>
-    styledNativeComponent(tag, css(strings, ...interpolations), { inline: true })
+    styledNativeComponent(tag, css(strings, ...interpolations))
 
-const aliases = {
-  get ActivityIndicator() { return styled(reactNative.ActivityIndicator) },
-  get ActivityIndicatorIOS() { return styled(reactNative.ActivityIndicatorIOS) },
-  get ART() { return styled(reactNative.ART) },
-  get DatePickerIOS() { return styled(reactNative.DatePickerIOS) },
-  get DrawerLayoutAndroid() { return styled(reactNative.DrawerLayoutAndroid) },
-  get Image() { return styled(reactNative.Image) },
-  get ImageEditor() { return styled(reactNative.ImageEditor) },
-  get ImageStore() { return styled(reactNative.ImageStore) },
-  get KeyboardAvoidingView() { return styled(reactNative.KeyboardAvoidingView) },
-  get ListView() { return styled(reactNative.ListView) },
-  get MapView() { return styled(reactNative.MapView) },
-  get Modal() { return styled(reactNative.Modal) },
-  get Navigator() { return styled(reactNative.Navigator) },
-  get NavigatorIOS() { return styled(reactNative.NavigatorIOS) },
-  get Picker() { return styled(reactNative.Picker) },
-  get PickerIOS() { return styled(reactNative.PickerIOS) },
-  get ProgressBarAndroid() { return styled(reactNative.ProgressBarAndroid) },
-  get ProgressViewIOS() { return styled(reactNative.ProgressViewIOS) },
-  get ScrollView() { return styled(reactNative.ScrollView) },
-  get SegmentedControlIOS() { return styled(reactNative.SegmentedControlIOS) },
-  get Slider() { return styled(reactNative.Slider) },
-  get SliderIOS() { return styled(reactNative.SliderIOS) },
-  get SnapshotViewIOS() { return styled(reactNative.SnapshotViewIOS) },
-  get Switch() { return styled(reactNative.Switch) },
-  get RecyclerViewBackedScrollView() { return styled(reactNative.RecyclerViewBackedScrollView) },
-  get RefreshControl() { return styled(reactNative.RefreshControl) },
-  get StatusBar() { return styled(reactNative.StatusBar) },
-  get SwipeableListView() { return styled(reactNative.SwipeableListView) },
-  get SwitchAndroid() { return styled(reactNative.SwitchAndroid) },
-  get SwitchIOS() { return styled(reactNative.SwitchIOS) },
-  get TabBarIOS() { return styled(reactNative.TabBarIOS) },
-  get Text() { return styled(reactNative.Text) },
-  get TextInput() { return styled(reactNative.TextInput) },
-  get ToastAndroid() { return styled(reactNative.ToastAndroid) },
-  get ToolbarAndroid() { return styled(reactNative.ToolbarAndroid) },
-  get Touchable() { return styled(reactNative.Touchable) },
-  get TouchableHighlight() { return styled(reactNative.TouchableHighlight) },
-  get TouchableNativeFeedback() { return styled(reactNative.TouchableNativeFeedback) },
-  get TouchableOpacity() { return styled(reactNative.TouchableOpacity) },
-  get TouchableWithoutFeedback() { return styled(reactNative.TouchableWithoutFeedback) },
-  get View() { return styled(reactNative.View) },
-  get ViewPagerAndroid() { return styled(reactNative.ViewPagerAndroid) },
-  get WebView() { return styled(reactNative.WebView) },
-  get div() { return styled('div') },
-}
+/* React native lazy-requires each of these modules for some reason, so let's
+*  assume it's for a good reason and not eagerly load them all */
+const aliases = `ActivityIndicator ActivityIndicatorIOS ART DatePickerIOS DrawerLayoutAndroid
+ Image ImageEditor ImageStore KeyboardAvoidingView ListView MapView Modal Navigator NavigatorIOS
+ Picker PickerIOS ProgressBarAndroid ProgressViewIOS ScrollView SegmentedControlIOS Slider
+ SliderIOS SnapshotViewIOS Switch RecyclerViewBackedScrollView RefreshControl StatusBar
+ SwipeableListView SwitchAndroid SwitchIOS TabBarIOS Text TextInput ToastAndroid ToolbarAndroid
+ Touchable TouchableHighlight TouchableNativeFeedback TouchableOpacity TouchableWithoutFeedback
+ View ViewPagerAndroid WebView`
+
+/* Define a getter for each alias which simply gets the reactNative component
+ * and passes it to styled */
+aliases.split(/\s+/m).forEach(alias => Object.defineProperty(styled, alias, {
+  enumerable: true,
+  configurable: false,
+  get() {
+    return styled(reactNative[alias])
+  },
+}))
 
 export { css }
-
-export default Object.create(styled, aliases)
+export default styled
--- src/types.js
+++ src/types.js
@@ -4,4 +4,5 @@ export type Interpolation = Function | Object | string
 /* todo: I want this to actually be an array of Function | string but that causes errors */
 export type RuleSet = Array<Interpolation>
 
+/* eslint-disable no-undef */
 export type Target = string | Class<Component>
