fa0548ab581a8c528d193736d17d6ba1439c5d84
Glen Maddern <glenmaddern@gmail.com>
2016-10-13 01:50:12 +1100

    starting this

--- src/models/ComponentStyle.js
+++ src/models/ComponentStyle.js
@@ -6,7 +6,6 @@ import type { RuleSet } from '../types'
 import flatten from '../utils/flatten'
 import parse from '../vendor/postcss-safe-parser/parse'
 import postcssNested from '../vendor/postcss-nested'
-import toEmoji from '../utils/toEmoji'
 import autoprefix from '../utils/autoprefix'
 
 const styleSheet = new StyleSheet({ speedy: false, maxLength: 40 })
@@ -16,35 +15,35 @@ const inserted = {}
  ComponentStyle is all the CSS-specific stuff, not
  the React-specific stuff.
  */
-export default class ComponentStyle {
-  rules: RuleSet;
-  insertedRule: Object;
+export default (classNameGenerator: (hash: Number) => string) =>
+  class ComponentStyle {
+    rules: RuleSet
+    insertedRule: Object
 
-  constructor(rules: RuleSet) {
-    this.rules = rules
-    if (!styleSheet.injected) styleSheet.inject()
-    this.insertedRule = styleSheet.insert('')
-  }
-
-  /*
-   * Flattens a rule set into valid CSS
-   * Hashes it, wraps the whole chunk in a ._hashName {}
-   * Parses that with PostCSS then runs PostCSS-Nested on it
-   * Returns the hash to be injected on render()
-   * */
-  generateAndInjectStyles(executionContext: Object) {
-    const flatCSS = flatten(this.rules, executionContext).join('')
-      .replace(/^\s*\/\/.*$/gm, '') // replace JS comments
-    const hash = hashStr(flatCSS)
-    if (!inserted[hash]) {
-      const selector = toEmoji(hash)
-      inserted[hash] = selector
-      const root = parse(`.${selector} { ${flatCSS} }`)
-      postcssNested(root)
-      autoprefix(root)
-      this.insertedRule.appendRule(root.toResult().css)
+    constructor(rules: RuleSet) {
+      this.rules = rules
+      if (!styleSheet.injected) styleSheet.inject()
+      this.insertedRule = styleSheet.insert('')
     }
-    return inserted[hash]
-  }
 
-}
+    /*
+     * Flattens a rule set into valid CSS
+     * Hashes it, wraps the whole chunk in a ._hashName {}
+     * Parses that with PostCSS then runs PostCSS-Nested on it
+     * Returns the hash to be injected on render()
+     * */
+    generateAndInjectStyles(executionContext: Object) {
+      const flatCSS = flatten(this.rules, executionContext).join('')
+        .replace(/^\s*\/\/.*$/gm, '') // replace JS comments
+      const hash = hashStr(flatCSS)
+      if (!inserted[hash]) {
+        const selector = classNameGenerator(hash)
+        inserted[hash] = selector
+        const root = parse(`.${selector} { ${flatCSS} }`)
+        postcssNested(root)
+        autoprefix(root)
+        this.insertedRule.appendRule(root.toResult().css)
+      }
+      return inserted[hash]
+    }
+  }
