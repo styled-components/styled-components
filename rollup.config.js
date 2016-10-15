4e3c4cedce3d6d7b69119bd5fbd6c89242354ac9
Rich-Harris <richard.a.harris@gmail.com>
2016-10-15 13:49:48 -0400

    switch to rollup

---
+++ rollup.config.js
@@ -0,0 +1,70 @@
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import inject from 'rollup-plugin-inject'
+import babel from 'rollup-plugin-babel'
+import json from 'rollup-plugin-json'
+import flow from 'rollup-plugin-flow'
+import uglify from 'rollup-plugin-uglify'
+
+const processShim = '\0process-shim'
+
+const prod = process.env.PRODUCTION
+const mode = prod ? 'production' : 'development'
+
+console.error(`creating ${mode} bundle...`)
+
+const targets = prod ?
+[
+  { dest: 'dist/styled-components.min.js', format: 'umd' },
+] :
+[
+  { dest: 'dist/styled-components.js', format: 'umd' },
+  { dest: 'dist/styled-components.es.js', format: 'es' },
+]
+
+const plugins = [
+  {
+    resolveId(importee) {
+      if (importee === processShim) return importee
+      return null
+    },
+    load(id) {
+      if (id === processShim) return 'export default { argv: [], env: {} }'
+      return null
+    },
+  },
+  flow(),
+  nodeResolve(),
+  commonjs(),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(prod ? 'production' : 'development'),
+  }),
+  inject({
+    process: processShim,
+  }),
+  babel({
+    babelrc: false,
+    presets: [
+      ['latest', { es2015: { modules: false } }],
+      'react',
+    ],
+    plugins: [
+      'transform-flow-strip-types',
+      'external-helpers',
+    ],
+  }),
+  json(),
+]
+
+if (prod) plugins.push(uglify())
+
+export default {
+  entry: 'src/index.js',
+  moduleName: 'styled',
+  external: ['react'],
+  exports: 'named',
+  targets,
+  plugins,
+  globals: { react: 'React' },
+}
