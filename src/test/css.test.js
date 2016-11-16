5f8c03745057cf94f990dfec3b592d36a67e1650
Glen Maddern <glenmaddern@gmail.com>
2016-11-17 09:41:02 +1100

    added failing test

--- src/test/css.test.js
+++ src/test/css.test.js
@@ -5,7 +5,7 @@ import { resetStyled, expectCSSMatches } from './utils'
 
 let styled
 
-describe('css features', () => {
+describe.only('css features', () => {
   beforeEach(() => {
     styled = resetStyled()
   })
@@ -16,6 +16,20 @@ describe('css features', () => {
     `
     shallow(<Comp />)
     expectCSSMatches('.a { -ms-transition: opacity 0.3s; -moz-transition: opacity 0.3s; -webkit-transition: opacity 0.3s; transition: opacity 0.3s; }')
+  })
+
+  it('should handle CSS calc()', () => {
+    const Comp = styled.div`
+      margin-bottom: calc(15px - 0.5rem) !important;
+    `
+    shallow(<Comp />)
+    expectCSSMatches(`
+      .a { 
+        margin-bottom: calc(15px - 0.5rem) !important;
+        marginBottom:-webkit-calc(15px - 0.5rem) !important;
+        marginBottom:-moz-calc(15px - 0.5rem) !important;
+      }
+    `)
   })
 
   it('should pass through custom properties', () => {
