f6438aff53dba35e2094b906d7f10feeaca3f924
Jacob Parker <jacobparker1992@gmail.com>
2016-11-28 17:41:02 +0000

    Add workarounds for more border properties

--- src/models/InlineStyle.js
+++ src/models/InlineStyle.js
@@ -26,24 +26,24 @@ export default class InlineStyle {
     if (!generated[hash]) {
       const root = parse(flatCSS)
       const declPairs = []
-      const workaroundValues = {}
       root.each(node => {
         if (node.type === 'decl') {
-          if (/border-?radius/i.test(node.prop)) {
-            // RN currently does not support differing values for the corner radii of Image
-            // components (but does for View). It is almost impossible to tell whether we'll have
-            // support, so we'll just disable multiple values here.
-            // https://github.com/styled-components/css-to-react-native/issues/11
-            workaroundValues.borderRadius = Number(node.value)
-          } else {
-            declPairs.push([node.prop, node.value])
-          }
+          declPairs.push([node.prop, node.value])
         } else {
           /* eslint-disable no-console */
           console.warn(`Node of type ${node.type} not supported as an inline style`)
         }
       })
-      const styleObject = Object.assign(transformDeclPairs(declPairs), workaroundValues)
+      // RN currently does not support differing values for the corner radii of Image
+      // components (but does for View). It is almost impossible to tell whether we'll have
+      // support, so we'll just disable multiple values here.
+      // https://github.com/styled-components/css-to-react-native/issues/11
+      const styleObject = transformDeclPairs(declPairs, [
+        'borderRadius',
+        'borderWidth',
+        'borderColor',
+        'borderStyle',
+      ])
       const styles = StyleSheet.create({
         generated: styleObject,
       })
