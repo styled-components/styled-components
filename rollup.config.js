af93825856da16d22ddaad261bc7418e80fa6298
Rolf Erik Lekang <me@rolflekang.com>
2016-12-18 11:52:08 +0100

    Fix lint errors from new rules

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable flowtype/require-valid-file-annotation, no-console */
 import nodeResolve from 'rollup-plugin-node-resolve'
 import replace from 'rollup-plugin-replace'
 import commonjs from 'rollup-plugin-commonjs'
