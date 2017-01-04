fabd53e9714367dbd20d6daa5133cde3d1f45a64
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2017-01-04 22:48:26 +0400

    mute flow errors

--- src/test/warnTooManyClasses.test.js
+++ src/test/warnTooManyClasses.test.js
@@ -10,46 +10,41 @@ let styled
 
 describe('warn too many classes', () => {
   const nativeWarn = console.warn
+  let warnCallCount;
   /**
    * Make sure the setup is the same for every test
    */
   beforeEach(() => {
+    (console: any).warn = () => warnCallCount++
+    warnCallCount = 0
     styled = resetStyled()
   })
 
   afterEach(() => {
-    console.warn = nativeWarn
+    (console: any).warn = nativeWarn
   })
 
   it('should warn once', () => {
-    let warnCalled = 0
-    console.warn = () => warnCalled++
     const Comp = styled.div`
       width: ${props => props.size};
     `
     for (let i = 0; i < 300; i++) {
       shallow(<Comp size={i}/>)
     }
-    expect(warnCalled).toEqual(1)
+    expect(warnCallCount).toEqual(1)
   })
 
   it('should warn if number of classes is 200', () => {
-    let warnCalled = 0
-
-    console.warn = () => warnCalled++
     const Comp = styled.div`
       width: ${props => props.size};
     `
     for (let i = 0; i < 200; i++) {
       shallow(<Comp size={i}/>)
     }
-    expect(warnCalled).toEqual(1)
+    expect(warnCallCount).toEqual(1)
   })
 
   it('should not warn if number of classes is below 200', () => {
-    let warnCalled = 0
-
-    console.warn = () => warnCalled++
     const Comp = styled.div`
       width: ${props => props.size};
     `
@@ -57,6 +52,6 @@ describe('warn too many classes', () => {
       shallow(<Comp size={i}/>)
     }
 
-    expect(warnCalled).toEqual(0)
+    expect(warnCallCount).toEqual(0)
   })
 })
