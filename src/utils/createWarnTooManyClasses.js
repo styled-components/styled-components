9feca81b10d0ee01a36eabbae858fa3afecedee0
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2017-01-04 22:10:18 +0400

    Update message

--- src/utils/createWarnTooManyClasses.js
+++ src/utils/createWarnTooManyClasses.js
@@ -15,9 +15,10 @@ export default () => {
         const latestRule = styleSheet.rules().find(s => s.selectorText && s.selectorText.indexOf(`.${className}`) !== -1)
         // Unable to find latestRule in test environment.
         /* eslint-disable no-console, prefer-template */
-        console.warn(`Over ${LIMIT} classes was generated for component ${displayName}. ` +
-           'Consider using React\'s style property for styling based on frequently changed. ' +
-           (latestRule ? `Last generated style: ${latestRule.cssText}` : ''))
+        console.warn(`Over ${LIMIT} classes were generated for component ${displayName}. ` +
+           'Consider using style property for frequently changed styles.\n' +
+           'Example: const MyStyledComponent = styled(props => <div className={props.className} style={{color: props.color}}>{props.children}</div>)`width: 100px; height: 100px;`' +
+           (latestRule ? `\nLatest generated class: ${latestRule.cssText}` : ''))
         warningSeen = true
         generatedClasses = {}
       }
