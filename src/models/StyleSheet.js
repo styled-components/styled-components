67f6889d695272031f41bdee3b1c88ab806affb2
chris <deezahyn@gmail.com>
2017-01-19 02:42:28 +0800

    add cache in SSR class
    - fixes #378

--- src/models/StyleSheet.js
+++ src/models/StyleSheet.js
@@ -7,8 +7,10 @@ import { StyleSheet as GlamorStyle } from '../vendor/glamor/sheet'
 
 class StyleSheet {
   styleSheet: GlamorStyle
+  cache: Array<{cssText: string}>
   constructor() {
     this.styleSheet = new GlamorStyle({ speedy: false, maxLength: 40 })
+    this.cache = []
   }
   get injected(): boolean {
     return this.styleSheet.injected
@@ -20,10 +22,19 @@ class StyleSheet {
     return this.styleSheet.insert(css)
   }
   reset() {
-    if (this.styleSheet.sheet) this.styleSheet.flush()
+    if (this.styleSheet.sheet) {
+      this.cache = this.styleSheet.rules()
+      this.styleSheet.flush()
+    }
   }
   getCSS({ min = true }: { min: boolean } = {}) {
-    return this.styleSheet.rules().map(rule => rule.cssText).join(min ? '' : '\n')
+    let css = this.styleSheet.rules()
+
+    if ((css.length <= 0) && (this.cache.length > 0)) {
+      css = this.cache
+    }
+
+    return css.map(rule => rule.cssText).join(min ? '' : '\n')
   }
 }
 
