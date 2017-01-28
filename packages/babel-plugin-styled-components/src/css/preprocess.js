d7e3e9f6915dec1f8e2a6717451fe6de1fd3d59c
Phil Pluckthun <phil.pluckthun@thetrainline.com>
2017-01-28 00:22:11 +0000

    Change into format that allows quick classname injection

--- packages/babel-plugin-styled-components/src/css/preprocess.js
+++ packages/babel-plugin-styled-components/src/css/preprocess.js
@@ -3,25 +3,65 @@ import stylis from 'stylis'
 // Matches strings that contain an opening curly brace on the same line
 const startsWithCurlyBrace = str => /^([^\n\r]+?)?\{/.test(str)
 
+// Generates a placeholder from an index
+const makePlaceholder = index => `__PLACEHOLDER_${index}__`
+
 // The capture group makes sure that the split contains the interpolation index
 const placeholderRegex = /__PLACEHOLDER_(\d+?)__/
+
+// Our temporary classname
+const temporaryClassname = '__TEMPORARY_CLASSNAME__'
 
 // Checks whether the CSS already contains something that matches our placeholders
 const containsPlaceholders = css => !!css.match(placeholderRegex)
 
+// Splits the css into an array with interleaved interpolation nodes
+const cssWithPlaceholdersToArr = (css, interpolationNodes) => {
+  const placeholderSplit = css.trim().split(placeholderRegex)
+  const res = []
+
+  for (let i = 0; i < placeholderSplit.length; i++) {
+    const str = placeholderSplit[i]
+    const isInterpolation = i % 2 !== 0
+
+    if (isInterpolation) {
+      const interpolationIndex = parseInt(str, 10)
+      res.push(interpolationNodes[interpolationIndex])
+    } else {
+      res.push(str)
+    }
+  }
+
+  return res
+}
+
+/*
+ * Flattens and splits CSS into an array where classname should be injected, and maps these
+ * partials to an array with interleaves interpolation nodes.
+ * Example:
+ * [
+ *   [ ':hover { color: blue; background:', props => props.background, '; }' ]
+ * ]
+ */
 const preprocess = (cssArr, ...interpolationNodes) => {
   let css = cssArr[0]
 
-  // Test whether the input is using the reserved placeholder format
-  if (css.some(containsPlaceholders)) {
+  // Test whether the input is using reserved strings
+  if (
+    css.some(x => (
+      containsPlaceholders(x) ||
+      x.includes(temporaryClassname)
+    ))
+  ) {
     throw new TypeError(
-      `CSS Input can't contain Styled Components placeholders of the format: __PLACEHOLDER_1__.`
+      `CSS Input can't contain Styled Components placeholders of the format: __PLACEHOLDER_1__ or __TEMPORARY_CLASSNAME__.`
     )
   }
 
+  // Assemble CSS partials and replace interpolations with placeholders
   for (let i = 1; i < cssArr.length) {
     const interpolationIndex = i - 1
-    const placeholder = `__PLACEHOLDER_${interpolationIndex}__`
+    const placeholder = makePlaceholder(interpolationIndex)
     const cssPartial = cssArr[i]
 
     // Append a semicolon to all interpolations except the onces that are for selectors
@@ -30,28 +70,15 @@ const preprocess = (cssArr, ...interpolationNodes) => {
     css += placeholder + suffix + cssPartial
   }
 
-  // Flatten CSS using stylis and split it by our placeholders
-  const processedCSS = stylis('&', css)
-  const processedCSSArr = processedCSS.split(placeholderRegex)
+  // Flatten CSS using stylis and split it by our temporary classname
+  const processedCSS = stylis(temporaryClassname, css)
 
-  const resCSSArr = []
-  const resInterpolationNodes = []
+  const classnameSplit = processedCSS
+    .split(temporaryClassname)
+    .filter(Boolean)
+    .map(str => cssWithPlaceholdersToArr(css, interpolationNodes))
 
-  for (let i = 0; i < processedCSSArr.length; i++) {
-    const str = processedCSSArr[i]
-    const isInterpolation = i % 2 !== 0
-
-    if (isInterpolation) {
-      // Guarantees that the resulting interpolations-array is in the right order
-      const interpolationIndex = parseInt(str, 10)
-      resInterpolationNodes.push(interpolationNodes[interpolationIndex])
-    } else {
-      resCSSArr.push(str)
-    }
-  }
-
-  // To be passed to t.callExpression
-  return [ resCSSArr, ...resInterpolationNodes ]
+  return classnameSplit
 }
 
 export default preprocess
