48de99a6a2546982d154473110915969e739ef52
Max <contact@mstoiber.com>
2017-03-14 08:59:21 -0700

    Disable the SSR option by default

--- packages/babel-plugin-styled-components/src/utils/options.js
+++ packages/babel-plugin-styled-components/src/utils/options.js
@@ -3,7 +3,7 @@ function getOption({ opts }, name, defaultValue = true) {
 }
 
 export const useDisplayName = (state) => getOption(state, 'displayName')
-export const useSSR = (state) =>  getOption(state, 'ssr')
+export const useSSR = (state) =>  getOption(state, 'ssr', false)
 export const useFileName = (state) =>getOption(state, 'fileName')
 export const useMinify = (state) => getOption(state, 'minify')
 export const useCSSPreprocessor = (state) => getOption(state, 'preprocess', false) // EXPERIMENTAL
