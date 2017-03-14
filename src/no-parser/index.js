86f6e984464301a86c4c85bbb112691841f0439e
Phil Plueckthun <phil@plckthn.me>
2017-03-14 23:44:26 +0000

    Turn constructWithOptions into a singleton factory taking css constructor

--- src/no-parser/index.js
+++ src/no-parser/index.js
@@ -10,12 +10,13 @@ import generateAlphabeticName from '../utils/generateAlphabeticName'
 import styleSheet from '../models/StyleSheet'
 
 /* Import singleton constructors */
-import _styledComponent from '../models/StyledComponent'
+import _StyledComponent from '../models/StyledComponent'
 import _ComponentStyle from '../models/ComponentStyle'
 import _GlobalStyle from '../models/GlobalStyle'
 import _styled from '../constructors/styled'
 import _keyframes from '../constructors/keyframes'
 import _injectGlobal from '../constructors/injectGlobal'
+import _constructWithOptions from '../constructors/constructWithOptions'
 
 /* Import components */
 import ThemeProvider from '../models/ThemeProvider'
@@ -25,11 +26,14 @@ import withTheme from '../hoc/withTheme'
 
 /* Instantiate singletons */
 const GlobalStyle = _GlobalStyle(flatten, stringifyRules)
+const ComponentStyle = _ComponentStyle(generateAlphabeticName, flatten, stringifyRules)
+const constructWithOptions = _constructWithOptions(css)
+const StyledComponent = _StyledComponent(ComponentStyle, constructWithOptions)
+
+/* Instantiate exported singletons */
 const keyframes = _keyframes(generateAlphabeticName, GlobalStyle, css)
 const injectGlobal = _injectGlobal(GlobalStyle, css)
-const styled = _styled(_styledComponent(
-  _ComponentStyle(generateAlphabeticName, flatten, stringifyRules),
-))
+const styled = _styled(StyledComponent, constructWithOptions)
 
 /* Export everything */
 export default styled
--- src/test/utils.js
+++ src/test/utils.js
@@ -6,10 +6,12 @@
 import expect from 'expect'
 
 import _styled from '../constructors/styled'
+import css from '../constructors/css'
+import _constructWithOptions from '../constructors/constructWithOptions'
 import styleSheet from '../models/StyleSheet'
 import flatten from '../utils/flatten'
 import stringifyRules from '../utils/stringifyRules'
-import _styledComponent from '../models/StyledComponent'
+import _StyledComponent from '../models/StyledComponent'
 import _ComponentStyle from '../models/ComponentStyle'
 
 /* Ignore hashing, just return class names sequentially as .a .b .c etc */
@@ -19,7 +21,12 @@ const classNames = () => String.fromCodePoint(97 + index++)
 export const resetStyled = () => {
   styleSheet.reset()
   index = 0
-  return _styled(_styledComponent(_ComponentStyle(classNames, flatten, stringifyRules)))
+
+  const ComponentStyle = _ComponentStyle(classNames, flatten, stringifyRules)
+  const constructWithOptions = _constructWithOptions(css)
+  const StyledComponent = _StyledComponent(ComponentStyle, constructWithOptions)
+
+  return _styled(StyledComponent, constructWithOptions)
 }
 
 const stripWhitespace = str => str.trim().replace(/([;\{\}])/g, '$1  ').replace(/\s+/g, ' ')
