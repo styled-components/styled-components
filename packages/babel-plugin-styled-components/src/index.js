b09ad86d1a03357256d15b57ebcba43e84a2b2bb
Phil Plueckthun <phil@plckthn.me>
2017-03-15 00:48:53 +0000

    Add import/require visitor for changing preprocess entrypoint

--- packages/babel-plugin-styled-components/src/index.js
+++ packages/babel-plugin-styled-components/src/index.js
@@ -1,10 +1,17 @@
 import minify from './visitors/minify'
 import displayNameAndId from './visitors/displayNameAndId'
 import templateLiterals from './visitors/templateLiterals'
+import { noParserImportDeclaration, noParserRequireCallExpression } from './visitors/noParserImport'
 
 export default function({ types: t }) {
   return {
     visitor: {
+      ImportDeclaration(path, state) {
+        noParserImportDeclaration(path, state)
+      },
+      CallExpression(path, state) {
+        noParserRequireCallExpression(path, state)
+      },
       TaggedTemplateExpression(path, state) {
         minify(path, state)
         displayNameAndId(path, state)
