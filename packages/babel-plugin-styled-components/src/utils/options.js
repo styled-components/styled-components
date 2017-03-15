dcf403393de61d12cc37d3d36db74186c203b69d
Phil Plueckthun <phil@plckthn.me>
2017-03-15 02:02:49 +0000

    Disable preprocessing by default

--- packages/babel-plugin-styled-components/src/utils/options.js
+++ packages/babel-plugin-styled-components/src/utils/options.js
@@ -6,5 +6,5 @@ export const useDisplayName = (state) => getOption(state, 'displayName')
 export const useSSR = (state) =>  getOption(state, 'ssr')
 export const useFileName = (state) =>getOption(state, 'fileName')
 export const useMinify = (state) => getOption(state, 'minify')
-export const useCSSPreprocessor = (state) => getOption(state, 'preprocess')
+export const useCSSPreprocessor = (state) => getOption(state, 'preprocess', false) // EXPERIMENTAL
 export const useTranspileTemplateLiterals = (state) => getOption(state, 'transpileTemplateLiterals')
