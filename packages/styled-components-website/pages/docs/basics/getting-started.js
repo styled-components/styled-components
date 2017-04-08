5d27d4f360ac306839398a019f3a88fc5cb18c0a
Phil Plueckthun <phil@plckthn.me>
2017-04-09 00:32:06 +0100

    Add "Passed Props" page

--- packages/styled-components-website/pages/docs/basics/getting-started.js
+++ packages/styled-components-website/pages/docs/basics/getting-started.js
@@ -3,12 +3,14 @@ import DocsLayout from '../../../components/DocsLayout'
 import LiveEdit from '../../../components/LiveEdit'
 
 const sample = (`
+// Create a Title component that'll render an <h1> tag with some styles
 const Title = styled.h1\`
   font-size: 1.5em;
   text-align: center;
   color: palevioletred;
 \`;
 
+// Create a Wrapper component that'll render a <section> tag with some styles
 const Wrapper = styled.section\`
   padding: 4em;
   background: papayawhip;
---
+++ packages/styled-components-website/pages/docs/basics/passed-props.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import DocsLayout from '../../../components/DocsLayout'
+import LiveEdit from '../../../components/LiveEdit'
+
+const sample = (`
+// Create an Input component that'll render an <input> tag with some styles
+const Input = styled.input\`
+  font-size: 1.25em;
+  padding: 0.5em;
+  margin: 0.5em;
+  color: palevioletred;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+
+  &:hover {
+    box-shadow: inset 1px 1px 2px rgba(0,0,0,0.1);
+  }
+\`;
+
+// Render a styled text input with a placeholder of "@mxstbr", and one with a value of "@geelen"
+render(
+  <div>
+    <Input placeholder="@mxstbr" type="text" />
+    <Input value="@geelen" type="text" />
+  </div>
+);
+`).trim()
+
+const PassedProps = ({ url }) => (
+  <DocsLayout url={url}>
+    <p>
+      Styled Components pass on all their props.
+    </p>
+
+    <p>
+      This example shows how all props of the Input component are passed on to the
+      DOM node that is mounted, as with React elements.
+    </p>
+
+    <LiveEdit
+      code={sample}
+      noInline
+    />
+  </DocsLayout>
+)
+
+export default PassedProps
