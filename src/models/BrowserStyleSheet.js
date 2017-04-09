3f3d90307e342138aa6fb246953e33cf05bd5228
Glen Maddern <glenmaddern@gmail.com>
2017-04-10 08:53:48 +1000

    refactoring, broke out a Tag class

--- src/models/BrowserStyleSheet.js
+++ src/models/BrowserStyleSheet.js
@@ -1,41 +1,60 @@
 // @flow
 /*
-* Browser Style Sheet with Rehydration
-*
-* <style data-styled-components-hashes="x y z">
-*   /· sc-component-id: a ·/
-*   .sc-a { ... }
-*   .x { ... }
-*   /· sc-component-id: b ·/
-*   .sc-b { ... }
-*   .y { ... }
-*   .z { ... }
-* </style>
-*
-* Note: replace · with * in the above snippet.
-* */
+ * Browser Style Sheet with Rehydration
+ *
+ * <style data-styled-components-hashes="x y z">
+ *   /· sc-component-id: a ·/
+ *   .sc-a { ... }
+ *   .x { ... }
+ *   /· sc-component-id: b ·/
+ *   .sc-b { ... }
+ *   .y { ... }
+ *   .z { ... }
+ * </style>
+ *
+ * Note: replace · with * in the above snippet.
+ * */
 import extractCompsFromCSS from '../utils/extractCompsFromCSS'
 
 const CSS_NAME = 'data-styled-components-hashes'
 const OBJ_NAME = 'styledComponentsHashes'
 
+class Tag {
+  el: HTMLStyleElement
+  components: Array<Object>
+
+  constructor(el, existingSource = '') {
+    this.el = el
+    this.components = extractCompsFromCSS(existingSource)
+  }
+}
+
 export class BrowserStyleSheet {
-  tags: Array<Object>
-  generatedHashes: Object
+  tags: Array<Tag>
+  generatedHashes: Set<string>
+  componentTags: Map<string, Tag>
 
   constructor() {
     this.initFromDOM()
+    this.constructComponentTagMap()
   }
 
   initFromDOM() {
     this.tags = []
-    this.generatedHashes = {}
+    this.generatedHashes = new Set()
     window.document.querySelector(`[${CSS_NAME}]`).forEach(el => {
       el.dataset[OBJ_NAME].split(' ').forEach(hash => {
-        this.generatedHashes[hash] = true
+        this.generatedHashes.add(hash, true)
       })
-      this.tags.push({ el, components: extractCompsFromCSS(el.innerHTML) })
+      this.tags.push(new Tag(el, el.innerHTML))
     })
+  }
+
+  constructComponentTagMap() {
+    this.componentTags = new Map()
+    this.tags.forEach(tag =>
+      tag.components.forEach(comp =>
+        this.componentTags.set(comp.componentId, tag)))
   }
 }
 
--- src/utils/extractCompsFromCSS.js
+++ src/utils/extractCompsFromCSS.js
@@ -1,6 +1,6 @@
 // @flow
 
-export default (css: ?String): Array<Object> => {
+export default (css: ?string): Array<Object> => {
   const [_, ...existingComponents] = (css || '').split(/^\/\* sc-component-id:/m)
   return (existingComponents || []).map(str => {
     const [componentId, componentCSS] = str.split(/\*\/$/m).map(s => s.trim())
