65f2d0e07a3fcf7c75e3b687776596b2187148ca
Glen Maddern <glenmaddern@gmail.com>
2017-04-11 19:10:09 +1000

    put the ordering bug back in for now

--- src/models/ComponentStyle.js
+++ src/models/ComponentStyle.js
@@ -8,17 +8,18 @@ import { StyleSheet } from './BrowserStyleSheet'
  ComponentStyle is all the CSS-specific stuff, not
  the React-specific stuff.
  */
+let counter = 0
 export default (nameGenerator: NameGenerator, flatten: Flattener, stringifyRules: Stringifier) => {
   class ComponentStyle {
     rules: RuleSet
     componentId: string
+    order: number
 
     constructor(rules: RuleSet, componentId: string) {
       this.rules = rules
       this.componentId = componentId
-
-      /* WELL SHIT */
-      // if (!StyleSheet.instance.hasInjectedComponent(componentId)) { StyleSheet.instance.inject(componentId, true, `.${componentId} {}`) }
+      // eslint-disable-next-line no-plusplus
+      this.order = counter++
     }
 
     /*
@@ -27,6 +28,11 @@ export default (nameGenerator: NameGenerator, flatten: Flattener, stringifyRules
      * Returns the hash to be injected on render()
      * */
     generateAndInjectStyles(executionContext: Object, styleSheet: StyleSheet) {
+      /* TODO: find a way for order to be taken in to account */
+      if (!styleSheet.hasInjectedComponent(this.componentId)) {
+        styleSheet.inject(this.componentId, true, `.${this.componentId} {}`)
+      }
+
       const flatCSS = flatten(this.rules, executionContext)
       const hash = hashStr(this.componentId + flatCSS.join(''))
 
