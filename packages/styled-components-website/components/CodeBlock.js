0bcd8aa88bab27fd1b8b2316f202412c95dc2c1d
Phil Plueckthun <phil@plckthn.me>
2017-04-11 04:40:20 +0100

    Fix CodeBlock not being selectable

--- packages/styled-components-website/components/CodeBlock.js
+++ packages/styled-components-website/components/CodeBlock.js
@@ -5,12 +5,13 @@ import { lightGrey } from '../utils/colors'
 import '../utils/prismTemplateString'
 import { Editor } from 'react-live'
 
-const CodeBlock = styled(Editor)`
+const CodeBlock = styled(Editor).attrs({
+  contentEditable: false
+})`
   background: ${lightGrey};
   font-size: 0.8rem;
   font-family: monospace;
   white-space: pre-wrap;
-  pointer-events: none;
 
   border-radius: ${rem(3)};
   box-shadow: 1px 1px 20px rgba(20, 20, 20, 0.27);
