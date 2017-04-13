6effbdfe149477068c1c391c3fda9e1bc0394ce9
Glen Maddern <glenmaddern@gmail.com>
2017-04-13 17:34:25 +1000

    omfg OWNED this is passing my ordering test too

--- src/test/ssr.test.js
+++ src/test/ssr.test.js
@@ -15,6 +15,30 @@ describe('ssr', () => {
   })
 
   it('should extract the CSS in a simple case', () => {
+    const Heading = styled.h1`
+      color: red;
+    `
+
+    const sheet = new ServerStyleSheet()
+    const html = renderToString(sheet.collectStyles(<Heading>Hello SSR!</Heading>))
+    const css = `
+${sheet.css.replace(/></g, ">\n<")}
+`
+
+    expect({ html, css }).toEqual({
+      html: '<h1 class="sc-a b" data-reactroot="" data-reactid="1" data-react-checksum="197727696">Hello SSR!</h1>',
+      css: `
+<style type="text/css" data-styled-components="b" data-styled-components-is-local="true">
+/* sc-component-id: sc-a */
+.sc-a {}
+.b {color: red;}
+
+</style>
+`,
+    })
+  })
+
+  it('should extract both global and local CSS', () => {
     injectGlobal`
       body { background: papayawhip; }
     `
@@ -25,7 +49,7 @@ describe('ssr', () => {
     const sheet = new ServerStyleSheet()
     const html = renderToString(sheet.collectStyles(<Heading>Hello SSR!</Heading>))
     const css = `
-${sheet.css.replace(/></g,">\n<")}
+${sheet.css.replace(/></g, ">\n<")}
 `
 
     expect({ html, css }).toEqual({
@@ -40,6 +64,41 @@ ${sheet.css.replace(/></g,">\n<")}
 /* sc-component-id: sc-a */
 .sc-a {}
 .b {color: red;}
+
+</style>
+`,
+    })
+  })
+
+  it('should render CSS in the order the components were defined, not rendered', () => {
+    const ONE = styled.h1.withConfig({ componentId: 'ONE' })`
+      color: red;
+    `
+    const TWO = styled.h2.withConfig({ componentId: 'TWO' })`
+      color: blue;
+    `
+
+    const sheet = new ServerStyleSheet()
+    const html = renderToString(sheet.collectStyles(
+      <div>
+        <TWO/>
+        <ONE/>
+      </div>
+    ))
+    const css = `
+${sheet.css.replace(/></g, ">\n<")}
+`
+
+    expect({ html, css }).toEqual({
+      html: '<div data-reactroot="" data-reactid="1" data-react-checksum="275982144"><h2 class="TWO a" data-reactid="2"></h2><h1 class="ONE b" data-reactid="3"></h1></div>',
+      css: `
+<style type="text/css" data-styled-components="a b" data-styled-components-is-local="true">
+/* sc-component-id: ONE */
+.ONE {}
+.b {color: red;}
+/* sc-component-id: TWO */
+.TWO {}
+.a {color: blue;}
 
 </style>
 `,
