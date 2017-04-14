63ee3b130303d5582d535c2f897d33138c48f102
Glen Maddern <glenmaddern@gmail.com>
2017-04-14 10:57:28 +1000

    simplifying the too many classes warning

--- src/utils/createWarnTooManyClasses.js
+++ src/utils/createWarnTooManyClasses.js
@@ -1,7 +1,4 @@
-/* eslint-disable flowtype/require-valid-file-annotation */
-/* TODO: restore flow on this file */
-
-import styleSheet from '../models/StyleSheet'
+// @flow
 
 const LIMIT = 200
 
@@ -13,15 +10,13 @@ export default (displayName: string) => {
     if (!warningSeen) {
       generatedClasses[className] = true
       if (Object.keys(generatedClasses).length >= LIMIT) {
-        const latestRule = styleSheet.rules().find(s => s.selectorText && s.selectorText.indexOf(`.${className}`) !== -1)
         // Unable to find latestRule in test environment.
         /* eslint-disable no-console, prefer-template */
         console.warn(`Over ${LIMIT} classes were generated for component ${displayName}. ` +
-           'Consider using style property for frequently changed styles.\n' +
-           'Example:\n' +
-           '  const StyledComp = styled.div`width: 100%;`\n' +
-           '  <StyledComp style={{ background: background }} />' +
-           (latestRule ? `\nLatest generated class: ${latestRule.cssText}` : ''))
+          'Consider using style property for frequently changed styles.\n' +
+          'Example:\n' +
+          '  const StyledComp = styled.div`width: 100%;`\n' +
+          '  <StyledComp style={{ background: background }} />')
         warningSeen = true
         generatedClasses = {}
       }
