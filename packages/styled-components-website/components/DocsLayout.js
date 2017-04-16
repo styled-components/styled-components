2184be31715f842f52e02cd2f76c513ecd29306d
Phil Plückthun <phil@plckthn.me>
2017-04-16 19:37:07 +0100

    Add adaptive title to DocsLayout

--- packages/styled-components-website/components/DocsLayout.js
+++ packages/styled-components-website/components/DocsLayout.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Head from 'next/head'
 import Navbar from './Navbar'
 import { Container, Content, Title } from './Layout'
 
 const DocsLayout = ({ children, title }) => (
   <Container>
+    <Head>
+      <title>Styled Components: {title}</title>
+    </Head>
+
     <Navbar />
 
     <Content>
