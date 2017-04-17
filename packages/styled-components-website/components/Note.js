b0cbb50fb08beaadfbcd6004fc96b3e89f46ac00
Phil Plückthun <phil@plckthn.me>
2017-04-17 03:39:15 +0100

    Remove absolute from Note label

--- packages/styled-components-website/components/Note.js
+++ packages/styled-components-website/components/Note.js
@@ -3,23 +3,19 @@ import rem from 'polished/lib/helpers/rem'
 import { lightVioletRed, violetRed } from '../utils/colors'
 
 const Note = styled.div`
-  position: relative;
-
   background: ${lightVioletRed};
-  padding: ${rem(35)} ${rem(10)} ${rem(10)} ${rem(14)};
+  padding: ${rem(7)} ${rem(10)} ${rem(10)} ${rem(14)};
   border-left: ${rem(4)} solid ${violetRed};
   margin: ${rem(20)} 0;
   border-radius: ${rem(3)}
 `
 
 const NoteLabel = styled.strong`
-  position: absolute;
-  top: ${rem(7)};
-
   display: block;
   font-weight: 600;
   text-transform: uppercase;
   font-size: 90%;
+  margin-bottom: ${rem(7)};
 `
 
 export default ({ label = 'Note', children }) => (
