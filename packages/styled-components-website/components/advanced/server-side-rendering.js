35dae71ad8151bc157fec521d80f6f8ccd1a971e
Phil Plückthun <phil@plckthn.me>
2017-04-30 20:18:40 +0100

    Update section on SSR for next.js

--- packages/styled-components-website/components/advanced/server-side-rendering.js
+++ packages/styled-components-website/components/advanced/server-side-rendering.js
@@ -59,6 +59,37 @@ export default class MyDocument extends Document {
 }
 `).trim()
 
+const nextSample = (`
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  render() {
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
+
+    return (
+      <html>
+        <Head>
+          {/* ... */}
+
+          {styleTags}
+        </Head>
+
+        <body>
+          <div className="root">
+            {main}
+          </div>
+
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
+`).trim()
+
 const ServerSideRendering = () => (
   <SectionLayout title="Server Side Rendering">
     <p>
