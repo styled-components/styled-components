245e4d859b3ad76dd024ebd680ca9d716a1ca047
drFabio <blackjackdevel@gmail.com>
2017-05-12 00:49:34 +0000

    Added sibling props failing tests

--- src/test/props.test.js
+++ src/test/props.test.js
@@ -1,7 +1,9 @@
 // @flow
 import React from 'react'
-import { shallow } from 'enzyme'
-
+import { mount, shallow } from 'enzyme'
+import jsdom from 'mocha-jsdom'
+import expect from 'expect'
+import styleSheet from '../models/StyleSheet'
 import { resetStyled, expectCSSMatches } from './utils'
 
 let styled
@@ -24,5 +26,33 @@ describe('props', () => {
     `
     shallow(<Comp fg="red"/>)
     expectCSSMatches('.a { color: red; }')
+  })
+  describe('sibling props', () => {
+    jsdom()
+    let Comp
+    beforeEach(() => {
+      styled = resetStyled()
+      Comp = styled.div`
+      color: ${props => props.red ? 'red' : 'white'}
+      & + & {
+        color: 'black'
+      }
+      `
+    })
+    it('Should give the same root class to the prop children and the propless one', ()=> {
+      const wrapper = mount(<div><Comp /><Comp red /></div>)
+      const comps = wrapper.find(Comp)
+      const proplessClass = comps.at(0).find('div').first().prop('className').split(' ')
+      const withPropClass = comps.at(1).find('div').first().prop('className').split(' ')
+      const atLeastOneClass = proplessClass.some((appliedClass) => (
+        appliedClass.indexOf(withPropClass) !== -1
+      ))
+      expect(atLeastOneClass).toEqual(true)
+    })
+    it.only('Should attach the sibling rule to the common root class', () => {
+      const wrapper = mount(<div><Comp /><Comp red /></div>)
+      console.log(styleSheet.rules().map(rule => rule.cssText).join('\n'))
+      expectCSSMatches('.root + .root { color: black; } .a { color: white; } .b { color: red}')
+    })
   })
 })
