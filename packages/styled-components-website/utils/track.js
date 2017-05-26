3ddccaaf12ca666484574f50265afcadce865553
Phil Plückthun <phil@plckthn.me>
2017-05-26 20:25:45 +0100

    Add a "Do Not Track" policy to tracking entrypoint

--- packages/styled-components-website/utils/track.js
+++ packages/styled-components-website/utils/track.js
@@ -1,20 +1,30 @@
 import fetch from 'isomorphic-fetch'
 import Router from 'next/router'
 
-const reportView = () => {
-  const { pathname } = window.location
+if (
+  (navigator.doNotTrack && navigator.doNotTrack !== 'no') ||
+  window.doNotTrack ||
+  navigator.msDoNotTrack
+) {
+  console.log('Tracking is disabled due to the Do Not Track policy of this browser.')
+} else {
+  console.log('Tracking is enabled. If you do not want your page view to be tracked enable the "Do Not Track" policy in your browser settings.')
 
-  fetch('https://sc-micro-analytics.now.sh' + pathname)
-    .then(() => {
-      console.log(`Reported page view for ${pathname}.`)
-    })
-    .catch(err => {
-      console.warn('Could not report page view:', err)
-    })
-}
+  const reportView = () => {
+    const { pathname } = window.location
 
-reportView()
+    fetch('https://sc-micro-analytics.now.sh' + pathname)
+      .then(() => {
+        console.log(`Reported page view for ${pathname}.`)
+      })
+      .catch(err => {
+        console.warn('Could not report page view:', err)
+      })
+  }
 
-Router.onRouteChangeComplete = () => {
   reportView()
+
+  Router.onRouteChangeComplete = () => {
+    reportView()
+  }
 }
