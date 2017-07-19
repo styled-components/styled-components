3840ed8c40e585aec0f2e80309992765dc090598
Amit Solanki <amit.fash@gmail.com>
2017-07-19 10:04:23 +0530

    Add Warning, when template literal strings are passed in wrong way

--- src/constructors/injectGlobal.js
+++ src/constructors/injectGlobal.js
@@ -5,6 +5,12 @@ import type { Interpolation, Stringifier } from '../types'
 
 export default (stringifyRules: Stringifier, css: Function) => {
   const injectGlobal = (strings: Array<string>, ...interpolations: Array<Interpolation>) => {
+    if (typeof strings === 'string') {
+        // eslint-disable-next-line no-console
+      console.warn(
+          'injectGlobal: Pass template literal strings without parentheses, global styles are not be applied. ',
+        )
+    }
     const rules = css(strings, ...interpolations)
     const hash = hashStr(JSON.stringify(rules))
 
