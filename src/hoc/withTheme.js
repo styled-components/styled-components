484bc071a1f8dad2cc1123f9707ad00fb9a0d2e9
Zach Smith <zach@dv01.co>
2017-08-01 10:07:50 -0400

    Custom withTheme implementation.

--- src/hoc/withTheme.js
+++ src/hoc/withTheme.js
@@ -1,18 +1,60 @@
 // @flow
 /* globals ReactClass */
-import hoistStatics from 'hoist-non-react-statics'
 import React from 'react'
-import { withTheme } from '../utils/theming'
+import hoistStatics from 'hoist-non-react-statics'
+import { themeListener } from '../utils/theming'
+import type { Theme } from '../models/ThemeProvider'
 import _isStyledComponent from '../utils/isStyledComponent'
+import getComponentName from '../utils/getComponentName'
 
-export default (Component: ReactClass<any>) => {
+export default (Component: ReactClass<mixed>) => {
   const isStyledComponent = _isStyledComponent(Component)
-  const WithTheme = withTheme(({ innerRef, ...props }) => (
-    <Component
-      {...props}
-      innerRef={isStyledComponent ? innerRef : undefined}
-      ref={isStyledComponent ? undefined : innerRef}
-    />
-  ))
+
+  class WithTheme extends React.Component {
+    static displayName = `withTheme(${getComponentName(Component)})`
+    static contextTypes = themeListener.contextTypes
+
+    state: {
+      theme: Theme
+    }
+    props: any
+
+    setTheme: (Theme) => void
+    unsubscribe: () => void
+
+    constructor(props, context) {
+      super(props, context)
+      this.state = {
+        theme: themeListener.initial(context),
+      }
+      this.setTheme = this.setTheme.bind(this)
+    }
+
+    setTheme(theme) {
+      this.setState({ theme })
+    }
+
+    componentDidMount() {
+      this.unsubscribe = themeListener.subscribe(this.context, this.setTheme)
+    }
+
+    componentWillUnmount() {
+      if (typeof this.unsubscribe === 'function') {
+        this.unsubscribe()
+      }
+    }
+
+    render() {
+      const { theme } = this.state
+      const { innerRef, ...props } = this.props
+
+      return (<Component
+        theme={theme}
+        {...props}
+        ref={isStyledComponent ? undefined : innerRef}
+        innerRef={isStyledComponent ? innerRef : undefined}
+      />)
+    }
+  }
   return hoistStatics(WithTheme, Component)
 }
--- src/utils/theming.js
+++ src/utils/theming.js
@@ -5,4 +5,4 @@ import { createTheming } from 'theming'
 // NOTE: DO NOT CHANGE, changing this is a semver major change!
 export const CHANNEL = '__styled-components__'
 
-export const { CHANNEL: chanel, withTheme, ThemeProvider, themeListener } = createTheming(CHANNEL)
+export const { CHANNEL: chanel, ThemeProvider, themeListener } = createTheming(CHANNEL)
