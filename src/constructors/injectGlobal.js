caa21fd18aa1c47f422d9588b3fabb0754ff744d
Roman Ponomarev <rvponomarev@yamoney.ru>
2017-10-02 22:06:40 +0300

    Fixes #352 Add warning for incorrect using injectGlobal

--- src/constructors/injectGlobal.js
+++ src/constructors/injectGlobal.js
@@ -5,6 +5,10 @@ import type { Interpolation, Stringifier } from '../types'
 
 export default (stringifyRules: Stringifier, css: Function) => {
   const injectGlobal = (strings: Array<string>, ...interpolations: Array<Interpolation>) => {
+    if (process.env.NODE_ENV !== 'production' && !(Array.isArray(strings))) {
+      // eslint-disable-next-line no-console
+      console.warn('Incorrect using injectGlobal as a function. Instead use it as a tagged template literal, see: https://www.styled-components.com/docs/api')
+    }
     const rules = css(strings, ...interpolations)
     const hash = hashStr(JSON.stringify(rules))
 
