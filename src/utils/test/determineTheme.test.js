c9961fd73cdb490e8c9a766a0988e597fd6fa5ed
kutyel <flaviocorpa@gmail.com>
2017-10-09 18:16:33 +0200

    Fix unit test 🚀

--- src/utils/test/determineTheme.test.js
+++ src/utils/test/determineTheme.test.js
@@ -3,13 +3,15 @@ import determineTheme from '../determineTheme';
 
 const theme = { color: 'red' }
 const fallbackTheme = { color: 'blue' }
+const props = { theme }
+const defaultProps = props
 
 describe('determineTheme', () => {
   it('should take precedence over ThemeProvider', () => {
-    expect(determineTheme({ theme }, fallbackTheme, { theme })).toEqual(theme);
+    expect(determineTheme(props, fallbackTheme, defaultProps)).toEqual(theme)
   })
 
   it('should fallback to default theme', () => {
-    expect(determineTheme({}, fallbackTheme, { theme })).toEqual(fallbackTheme)
+    expect(determineTheme({}, fallbackTheme, defaultProps)).toEqual(fallbackTheme)
   })
 })
