0b86440a988b3c7b3b1a490c55b5b78055f59d28
Brian Pedersen <bpedersen@i3logix.com>
2017-10-20 08:36:25 -0600

    added dependencies and removed the copied folder from babel-plugin-styled-components

--- src/macro/index.js
+++ src/macro/index.js
@@ -1,13 +1,13 @@
-/* eslint-disable */
+// @flow
 import { createMacro } from 'babel-macros'
-import * as t from 'babel-types'
-import minify from './visitors/minify'
-import displayNameAndId from './visitors/displayNameAndId'
-import templateLiteral from './visitors/templateLiterals'
+import minify from 'babel-plugin-styled-components/lib/visitors/minify'
+import displayNameAndId from 'babel-plugin-styled-components/lib/visitors/displayNameAndId'
+import templateLiteral from 'babel-plugin-styled-components/lib/visitors/templateLiterals'
 
 const configName = 'styledComponents'
 
 function styledComponentsMacro({ references, state, babel, config = {} }) {
+  const t = babel.types
   // add 'import styled from styled-components'
   const literal = config.preprocess ? 'styled-components/no-parser' : 'styled-components'
   state.file.path.parent.program.body.unshift(
@@ -18,13 +18,17 @@ function styledComponentsMacro({ references, state, babel, config = {} }) {
   )
   // iterate over each call and modify the TaggedTemplateExpression
   references.default.forEach(referencePath => {
-    const path = referencePath.parentPath.parentPath
-    referencePath.node.name = 'styled'
-    state.opts = config
-    if (path.type === 'TaggedTemplateExpression') {
-      minify(path, state)
-      displayNameAndId(path, state)
-      templateLiteral(path, state)
+    const templatePath = referencePath.parentPath.parentPath
+    const basePath = referencePath
+    basePath.node.name = 'styled'
+    const newState = {
+      ...state,
+      opts: config,
+    }
+    if (templatePath.type === 'TaggedTemplateExpression') {
+      minify(templatePath, newState)
+      displayNameAndId(templatePath, newState)
+      templateLiteral(templatePath, newState)
     }
   })
 }
