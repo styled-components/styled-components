567e456ffc8d362c81a3e5aaa106debcc14d72a0
Evan Scott <evan.scott@zocdoc.com>
2017-11-17 15:33:32 -0500

    add TODO comment for implementing CSS.escape eventually

--- src/utils/escape.js
+++ src/utils/escape.js
@@ -2,6 +2,10 @@
 const escapeRegex = /[[\].#*$><+~=|^:(),"'`-]+/g
 const dashesAtEnds = /(^-|-$)/g
 
+/**
+ * TODO: Explore using CSS.escape when it becomes more available
+ * in evergreen browsers.
+ */
 export default function escape(str: string): string {
   return str
         // Replace all possible CSS selectors
