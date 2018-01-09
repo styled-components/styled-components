a49dc8118ad33c42706a7df5d5123ba70d21175b
Jamie Dixon <jamie@jamie-dixon.co.uk>
2018-01-09 16:23:20 +0000

    Check context for stylesheet instance

--- src/constructors/createGlobalStyle.js
+++ src/constructors/createGlobalStyle.js
@@ -1,27 +1,34 @@
 // @flow
 import React from 'react'
+import PropTypes from 'prop-types'
 import hashStr from '../vendor/glamor/hash'
-import StyleSheet from '../models/StyleSheet'
+import StyleSheet, { CONTEXT_KEY } from '../models/StyleSheet'
+import ServerStyleSheet from '../models/ServerStyleSheet'
 import type { Interpolation, Stringifier } from '../types'
 
 const _createGlobalStyle = (stringifyRules: Stringifier, css: Function) => {
   const createGlobalStyle = (strings: Array<string>, ...interpolations: Array<Interpolation>) => {
     return class extends React.Component {
+      static contextTypes = {
+        [CONTEXT_KEY]: PropTypes.oneOfType([PropTypes.instanceOf(StyleSheet), PropTypes.instanceOf(ServerStyleSheet)]),
+      }
       componentId = ''
 
       componentWillMount() {
         const rules = css(strings, ...interpolations)
         const hash = hashStr(JSON.stringify(rules))
+        const styleSheet = this.context[CONTEXT_KEY] || StyleSheet.instance
         this.componentId = `sc-global-${hash}`
 
-        if (StyleSheet.instance.hasInjectedComponent(this.componentId)) return
+        if (styleSheet.hasInjectedComponent(this.componentId)) return
 
-        StyleSheet.instance.inject(this.componentId, false, stringifyRules(rules))
+        styleSheet.inject(this.componentId, false, stringifyRules(rules))
       }
 
       componentWillUnmount() {
-        if (!StyleSheet.instance.hasInjectedComponent(this.componentId)) return
-        StyleSheet.instance.removeComponent(this.componentId)
+        const styleSheet = this.context[CONTEXT_KEY] || StyleSheet.instance
+        if (!styleSheet.hasInjectedComponent(this.componentId)) return
+        styleSheet.removeComponent(this.componentId)
       }
 
       render() {
