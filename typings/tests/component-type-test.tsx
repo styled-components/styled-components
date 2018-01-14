cd9dac9f854da13c50885713b94aadaaab4502c0
Tom Crockett <pelotom@gmail.com>
2018-01-13 18:37:52 -0800

    Add link to the TypeScript bug report

--- typings/tests/component-type-test.tsx
+++ typings/tests/component-type-test.tsx
@@ -11,4 +11,5 @@ styled(C); // used to fail; see issue trail linked below
 // https://github.com/mui-org/material-ui/pull/8781#issuecomment-349460247
 // https://github.com/mui-org/material-ui/issues/9838
 // https://github.com/styled-components/styled-components/pull/1420
+// https://github.com/Microsoft/TypeScript/issues/21175
 // https://github.com/styled-components/styled-components/pull/1427
