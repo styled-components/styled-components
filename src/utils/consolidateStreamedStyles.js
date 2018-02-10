887c03339d58942f4878ac014ce8421f7299d022
Phil Plückthun <phil@plckthn.me>
2018-02-10 22:46:31 +0000

    Add deprecation note and warning to consolidateStreamedStyles

--- src/utils/consolidateStreamedStyles.js
+++ src/utils/consolidateStreamedStyles.js
@@ -1,11 +1,18 @@
 // @flow
-/**
- * When using streaming rendering, style blocks are emitted in chunks directly.
- * This method used to be necessary to move all style elements before the
- * rehydration kicks in. However the rehydration now takes all SSR style tags,
- * combines them into a single tag, and adds it to the head.
- * This renders the consolidation useless
- */
+
+/* This function is DEPRECATED and will be removed on the next major version release.
+ * It was needed to rehydrate all style blocks prepended to chunks before React
+ * tries to rehydrate its HTML stream. Since the master StyleSheet will now detect
+ * the use of streamed style tags and will perform the rehydration earlier when needed
+ * this function will not be needed anymore */
 export default function consolidateStreamedStyles() {
-  /* noop */
+  if (process.env.NODE_ENV !== 'production') {
+    // eslint-disable-next-line no-console
+    console.warn(
+      'The consolidateStreamedStyles function for streamed SSR rehydration was removed.\n' +
+        'The rehydration will now be automatically performed at an earlier point in time ' +
+        'on the client so that this function is now not needed anymore and is thus obsolete.\n' +
+        '- Consider removing this call on the client and use React + styled-components as you would expect'
+    )
+  }
 }
