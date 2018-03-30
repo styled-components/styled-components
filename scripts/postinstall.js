37b43908f0702f0cbf7b765a833356bcbb7d934a
Phil Plückthun <phil@kitten.sh>
2018-03-30 14:25:37 +0100

    Remove opencollective cli and add shorter postinstall msg

---
+++ scripts/postinstall.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+/* eslint-disable */
+/* Adapted from nodemon's postinstall: https://github.com/remy/nodemon/blob/master/package.json */
+
+var msg = 'Take a look at https://opencollective.com/styled-components/donate to help out styled-components’ maintainers';
+
+console.log(msg);
