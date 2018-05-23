31fa545bc7a46141beca5a64609929622db7987e
Mickael Lecoq <m.blacksad@gmail.com>
2018-05-23 17:05:43 +0200

    support x attributes

--- src/utils/validAttr.js
+++ src/utils/validAttr.js
@@ -23,7 +23,7 @@ const ATTRIBUTE_NAME_START_CHAR =
   ':A-Z_a-z\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02FF\\u0370-\\u037D\\u037F-\\u1FFF\\u200C-\\u200D\\u2070-\\u218F\\u2C00-\\u2FEF\\u3001-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFFD'
 const ATTRIBUTE_NAME_CHAR = `${ATTRIBUTE_NAME_START_CHAR}\\-.0-9\\u00B7\\u0300-\\u036F\\u203F-\\u2040`
 const isCustomAttribute = RegExp.prototype.test.bind(
-  new RegExp(`^(data|aria)-[${ATTRIBUTE_NAME_CHAR}]*$`)
+  new RegExp(`^(x|data|aria)-[${ATTRIBUTE_NAME_CHAR}]*$`)
 )
 
 export default (name: string) =>
