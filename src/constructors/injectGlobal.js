c856afb39d050bb6bc364c42512bd3a81521b47e
Evan Scott <probablyup@gmail.com>
2018-08-11 10:38:58 -0500

    add warnings for the removal of extend and injectGlobal in v4

--- src/constructors/injectGlobal.js
+++ src/constructors/injectGlobal.js
@@ -1,12 +1,23 @@
 // @flow
 import hashStr from '../vendor/glamor/hash'
 import StyleSheet from '../models/StyleSheet'
+import once from '../utils/once'
 import type { Interpolation, Stringifier } from '../types'
 
 type InjectGlobalFn = (
   strings: Array<string>,
   ...interpolations: Array<Interpolation>
 ) => void
+
+let warnInjectGlobalDeprecated
+if (process.env.NODE_ENV !== 'production') {
+  warnInjectGlobalDeprecated = once(() => {
+    // eslint-disable-next-line no-console
+    console.error(
+      'Notice: The "injectGlobal" API has been marked for removal in favor of the upcoming "createGlobalStyle" API that will be available in v4.0+. More info here: https://github.com/styled-components/styled-components/issues/1333'
+    )
+  })
+}
 
 export default (stringifyRules: Stringifier, css: Function) => {
   const injectGlobal: InjectGlobalFn = (...args) => {
@@ -18,6 +29,8 @@ export default (stringifyRules: Stringifier, css: Function) => {
     if (!styleSheet.hasId(id)) {
       styleSheet.inject(id, stringifyRules(rules))
     }
+
+    warnInjectGlobalDeprecated()
   }
 
   return injectGlobal
