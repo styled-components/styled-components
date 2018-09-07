e0986cc2eb98669b0bdcf3736b164b79800bb1f2
ka2jun8 <ka210n8@gmail.com>
2018-09-07 23:21:10 +0900

    add test to ssr.test for ssr chunked streaming

--- src/test/ssr.test.js
+++ src/test/ssr.test.js
@@ -269,6 +269,60 @@ describe('ssr', () => {
     })
   })
 
+  it('should interleave styles with rendered HTML when chunked streaming', () => {
+    injectGlobal`
+      body { background: papayawhip; }
+    `
+    const Heading = styled.h1`
+      color: red;
+    `
+
+    const Body = styled.div`
+      color: blue;
+    `
+
+    const SideBar = styled.div`
+      color: yellow;
+    `
+
+    const Footer = styled.div`
+      color: green;
+    `
+
+    const sheet = new ServerStyleSheet()
+    const jsx = sheet.collectStyles(
+      <div>
+        <Heading>Hello SSR!</Heading>
+        <Body>
+        {new Array(1000)
+          .fill(0)
+          .map(() => <div>*************************</div>)}
+        </Body>
+        <SideBar>SideBar</SideBar>
+        <Footer>Footer</Footer>
+      </div>
+    )
+    const stream = sheet.interleaveWithNodeStream(renderToNodeStream(jsx))
+
+    return new Promise((resolve, reject) => {
+      let received = ''
+
+      stream.on('data', chunk => {
+        received += chunk
+      })
+
+      stream.on('end', () => {
+        expect(received).toMatchSnapshot()
+        expect(sheet.closed).toBe(true)
+        expect(received).toMatch(/yellow/)
+        expect(received).toMatch(/green/)
+        resolve()
+      })
+
+      stream.on('error', reject)
+    })
+  })
+
   it('should handle errors while streaming', () => {
     const Component = createGlobalStyle`
       body { background: papayawhip; }
