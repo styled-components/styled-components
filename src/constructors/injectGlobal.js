5480e58e86c920cc3216d840ac83f09353a96fa0
Evan Scott <probablyup@gmail.com>
2018-09-18 09:39:43 -0500

    remove injectGlobal deprecation warning
    It's not actionable since the replacement API is in the next
    major library version... so yeah.

--- src/constructors/injectGlobal.js
+++ src/constructors/injectGlobal.js
@@ -1,23 +1,12 @@
 // @flow
 import hashStr from '../vendor/glamor/hash'
 import StyleSheet from '../models/StyleSheet'
-import once from '../utils/once'
 import type { Interpolation, Stringifier } from '../types'
 
 type InjectGlobalFn = (
   strings: Array<string>,
   ...interpolations: Array<Interpolation>
 ) => void
-
-let warnInjectGlobalDeprecated
-if (process.env.NODE_ENV !== 'production') {
-  warnInjectGlobalDeprecated = once(() => {
-    // eslint-disable-next-line no-console
-    console.warn(
-      'Notice: The "injectGlobal" API will be removed in the upcoming v4.0 release. Use "createGlobalStyle" instead. You can find more information here: https://github.com/styled-components/styled-components/issues/1333'
-    )
-  })
-}
 
 export default (stringifyRules: Stringifier, css: Function) => {
   const injectGlobal: InjectGlobalFn = (...args) => {
@@ -29,8 +18,6 @@ export default (stringifyRules: Stringifier, css: Function) => {
     if (!styleSheet.hasId(id)) {
       styleSheet.inject(id, stringifyRules(rules))
     }
-
-    if (warnInjectGlobalDeprecated) warnInjectGlobalDeprecated()
   }
 
   return injectGlobal
