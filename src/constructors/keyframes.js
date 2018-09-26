12afcb3006469f8df1c7affc43557cc26a69a2e4
Viktor Havrylin <vhavrylin@gmail.com>
2018-09-27 00:33:34 +0300

    Add warning when using `keyframes` on React Native

--- src/constructors/keyframes.js
+++ src/constructors/keyframes.js
@@ -14,6 +14,15 @@ export default function keyframes(
   strings: Styles,
   ...interpolations: Array<Interpolation>
 ): Keyframes {
+  /* Warning if you've used keyframes on React Native */
+  if (
+    process.env.NODE_ENV !== 'production' &&
+    typeof navigator !== 'undefined' &&
+    navigator.product === 'ReactNative'
+  ) {
+    console.warn('Helper `keyframes` cannot be used on React Native');
+  }
+
   const rules = css(strings, ...interpolations);
 
   const name = generateAlphabeticName(hashStr(replaceWhitespace(JSON.stringify(rules))));
