3244b37ddc5fed6a4448541ecf3bc642e93a2550
Tal Woolf <tal@square-radius.com>
2018-09-30 19:27:48 +0100

    determinTheme/updated defaultProps to use EMPTY_OBJECT

--- src/utils/determineTheme.js
+++ src/utils/determineTheme.js
@@ -1,4 +1,6 @@
 // @flow
+import { EMPTY_OBJECT } from './empties';
+
 type Props = {
   theme?: any,
 };
@@ -7,10 +9,12 @@ export default (props: Props, fallbackTheme: any, defaultProps: any) => {
   // Props should take precedence over ThemeProvider, which should take precedence over
   // defaultProps, but React automatically puts defaultProps on props.
 
-  /* eslint-disable react/prop-types */
+  /* eslint-disable react/prop-types, flowtype-errors/show-errors */
   const isDefaultTheme = defaultProps ? props.theme === defaultProps.theme : false;
-  const defaultPropsTheme = (defaultProps || {}).theme;
-  const theme = props.theme && !isDefaultTheme ? props.theme : fallbackTheme || defaultPropsTheme;
+  const theme =
+    props.theme && !isDefaultTheme
+      ? props.theme
+      : fallbackTheme || (defaultProps || EMPTY_OBJECT).theme;
   /* eslint-enable */
 
   return theme;
