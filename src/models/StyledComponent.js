77dd2ad383b1fc2ac1b71e24ae6e540b95abc458
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-10-01 23:09:46 +0200

    Forward innerRef prop

--- src/models/StyledComponent.js
+++ src/models/StyledComponent.js
@@ -98,12 +98,14 @@ class StyledComponent extends PureComponent<*> {
 
     let key;
     for (key in this.props) {
-      if (key === 'forwardedClass' || key === 'as') continue;
-      else if (process.env.NODE_ENV !== 'production' && key === 'innerRef') {
+      if (process.env.NODE_ENV !== 'production' && key === 'innerRef') {
         warnInnerRef();
-      } else if (key === 'forwardedRef') propsForElement.ref = this.props[key];
-      // Don't pass through non HTML tags through to HTML elements
+      }
+
+      if (key === 'forwardedClass' || key === 'as') continue;
+      else if (key === 'forwardedRef') propsForElement.ref = this.props[key];
       else if (!isTargetTag || validAttr(key)) {
+        // Don't pass through non HTML tags through to HTML elements
         propsForElement[key] =
           key === 'style' && key in this.attrs
             ? { ...this.attrs[key], ...this.props[key] }
