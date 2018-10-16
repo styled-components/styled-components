36c24257295c375276bad77e6a7bd56f970b0ca6
yiochen <chen.yiou1992@gmail.com>
2018-10-15 20:41:03 -0700

    fixed sourcemap unit test

--- src/test/sourceMap.test.js
+++ src/test/sourceMap.test.js
@@ -1,25 +1,24 @@
 // @flow
-import React from 'react'
-import { resetStyled } from './utils'
-import { shallow } from 'enzyme'
-import { join } from 'path'
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import { resetStyled } from './utils';
 
-let styled
+let styled;
 
 describe('sourceMap', () => {
   beforeEach(() => {
-    styled = resetStyled()
-  })
+    styled = resetStyled();
+  });
 
   it('should inject sourceMap', () => {
-    const fakeSourceMap = '/* fake source map */'
+    const fakeSourceMap = '/* fake source map */';
     const Named1 = styled.div.withConfig({
       sourceMap: fakeSourceMap,
-    })``
-    shallow(<Named1 />)
+    })``;
+    TestRenderer.create(<Named1 />);
     const allStyles = Array.from(document.querySelectorAll('style'))
       .map(tag => tag.innerHTML)
-      .join('\n')
-    expect(allStyles.includes(fakeSourceMap))
-  })
-})
+      .join('\n');
+    expect(allStyles.includes(fakeSourceMap));
+  });
+});
