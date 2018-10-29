ba3dbde2c5de477984f77aa3d02d9662f1ef64a4
Max Stoiber <contact@mxstbr.com>
2018-10-29 13:16:09 +0100

    Fix assigning StyleSheet

--- src/models/StyledComponent.js
+++ src/models/StyledComponent.js
@@ -73,12 +73,11 @@ class StyledComponent extends Component<*> {
   }
 
   renderOuter(styleSheet?: StyleSheet) {
+    this.styleSheet = styleSheet;
     const { componentStyle } = this.props.forwardedClass;
     
     // No need to subscribe a static component to theme changes, it won't change anything
     if (componentStyle.isStatic) return this.renderInner();
-
-    this.styleSheet = styleSheet;
 
     return <ThemeConsumer>{this.renderInner}</ThemeConsumer>;
   }
