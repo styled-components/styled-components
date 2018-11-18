701e77e6f542b6e27fbb9ae0b926e2b4524d2766
Alexey Svetliakov <asvetliakov@teledini.com>
2018-11-18 05:28:43 +0100

    isTag() additionally check for casing of the first char

--- src/utils/getComponentName.js
+++ src/utils/getComponentName.js
@@ -1,6 +1,6 @@
 // @flow
 import type { ComponentType } from 'react';
 
-export default function getComponentName(target: ComponentType<*>): string {
-  return target.displayName || target.name || 'Component';
+export default function getComponentName(target: ComponentType<*> | string): string {
+  return typeof target === "string" ? target : target.displayName || target.name || 'Component';
 }
--- src/utils/isTag.js
+++ src/utils/isTag.js
@@ -2,5 +2,5 @@
 import type { Target } from '../types';
 
 export default function isTag(target: Target) /* : %checks */ {
-  return typeof target === 'string';
+  return typeof target === 'string' && target.charAt(0) === target.charAt(0).toLowerCase();
 }
