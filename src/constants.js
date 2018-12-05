32fc9138ca2622970746834f92c7a952535809de
Brian Lenz <brian@socialstrata.com>
2018-12-05 08:35:51 -0500

    Removed unused constant and new lines

--- src/constants.js
+++ src/constants.js
@@ -6,9 +6,7 @@ declare var SC_DISABLE_SPEEDY: ?boolean
 export const SC_ATTR =
   (typeof process !== 'undefined' && process.env.SC_ATTR) ||
   'data-styled-components'
-
 export const SC_STREAM_ATTR = 'data-styled-streamed'
-
 export const CONTEXT_KEY = '__styled-components-stylesheet__'
 
 export const IS_BROWSER =
@@ -18,6 +16,3 @@ export const DISABLE_SPEEDY =
   (typeof __DEV__ === 'boolean' && __DEV__) ||
   (typeof SC_DISABLE_SPEEDY === 'boolean' && SC_DISABLE_SPEEDY) ||
   process.env.NODE_ENV !== 'production'
-
-// Shared empty execution context when generating static styles
-export const STATIC_EXECUTION_CONTEXT = {}
