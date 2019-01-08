e0abf3a23a34f8247b4b4f76aba2ed5bd9bad9cf
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-01-08 13:33:16 +0100

    Loosen hnrs flow types
    Current react typings do not include e.g. React.forwardRef as a valid component
    type.

--- flow-typed/npm/hoist-non-react-statics_v3.x.x.js
+++ flow-typed/npm/hoist-non-react-statics_v3.x.x.js
@@ -7,9 +7,9 @@ declare module 'hoist-non-react-statics' {
     TP - target component props
     SP - additional source component props
   */
-  declare module.exports: <TP, SP, S>(
-    target: React$ComponentType<TP>,
-    source: React$ComponentType<SP> & S,
-    blacklist?: { [key: $Keys<S>]: boolean }
-  ) => React$ComponentType<TP> & $Shape<S>;
+  declare module.exports: <T>(
+    target: T,
+    source: any,
+    blacklist?: {}
+  ) => T;
 }
