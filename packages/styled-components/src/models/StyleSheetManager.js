3b4e8f7cdc23468d0c4dd67131c682d571308d65
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-01-24 17:42:21 +0100

    Minor refactor

--- packages/styled-components/src/models/StyleSheetManager.js
+++ packages/styled-components/src/models/StyleSheetManager.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { createContext, useMemo, type Element } from 'react';
+import React, { useMemo, type Element } from 'react';
 import PropTypes from 'prop-types';
 import StyleSheet from './StyleSheet';
 import ServerStyleSheet from './ServerStyleSheet';
@@ -11,26 +11,25 @@ type Props = {
   target?: HTMLElement,
 };
 
-export const StyleSheetContext = createContext();
+export const StyleSheetContext = React.createContext();
 
 export const StyleSheetConsumer = StyleSheetContext.Consumer;
 
+function getSheet(sheet?: StyleSheet, target?: HTMLElement): StyleSheet {
+  if (sheet) {
+    return sheet;
+  } else if (target) {
+    return new StyleSheet(target);
+  } else {
+    throw new StyledError(4);
+  }
+}
+
 export default function StyleSheetManager(props: Props) {
-  const styleSheet = useMemo(
-    () => {
-      if (props.sheet) {
-        return props.sheet;
-      } else if (props.target) {
-        return new StyleSheet(props.target);
-      } else {
-        throw new StyledError(4);
-      }
-    },
-    [props.sheet, props.target]
-  );
+  const sheet = useMemo(() => getSheet(props.sheet, props.target), [props.sheet, props.target]);
 
   return (
-    <StyleSheetContext.Provider value={styleSheet}>
+    <StyleSheetContext.Provider value={sheet}>
       {process.env.NODE_ENV !== 'production' ? React.Children.only(props.children) : props.children}
     </StyleSheetContext.Provider>
   );
