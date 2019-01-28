973c6b561f2837cc82e20600bc1fdaa3959d0d49
Alexander Nanberg <alexander.nanberg@strateg.se>
2019-01-28 12:08:29 +0100

    Remove transform runtime without breaking jest

--- babel.config.js
+++ babel.config.js
@@ -4,11 +4,16 @@ const modules = BABEL_ENV === 'cjs' || NODE_ENV === 'test' ? 'commonjs' : false;
 const loose = true;
 
 module.exports = api => {
-  api.cache(true);
-
   return {
     presets: [
-      ['@babel/preset-env', { loose, modules }],
+      [
+        '@babel/preset-env',
+        {
+          loose,
+          modules,
+          ...(api.env('test') ? { targets: { node: 'current' } } : {}),
+        },
+      ],
       '@babel/preset-react',
       '@babel/preset-flow',
     ],
@@ -16,13 +21,6 @@ module.exports = api => {
       'preval',
       '@babel/plugin-transform-flow-strip-types',
       ['@babel/plugin-proposal-class-properties', { loose }],
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          helpers: false,
-          regenerator: true,
-        },
-      ],
       ['transform-react-remove-prop-types', { mode: 'unsafe-wrap' }],
       modules === 'commonjs' && 'add-module-exports',
     ].filter(Boolean),
