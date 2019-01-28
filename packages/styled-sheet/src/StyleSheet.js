f09873194e05e687355788a035f45aa40168e42a
Phil Pluckthun <phil@kitten.sh>
2019-01-28 02:53:09 +0000

    Readd forceServer argument to StyleSheet

--- packages/styled-sheet/src/StyleSheet.js
+++ packages/styled-sheet/src/StyleSheet.js
@@ -2,32 +2,32 @@
 
 import type { GroupedTag, GroupedKeys } from './types';
 import { IS_BROWSER } from './constants';
-import { RuleGroupTag, DefaultTag } from './tags';
+import { RuleGroupTag, DefaultTag, VirtualTag } from './tags';
 import { makeCssMarker } from './utils';
 import GroupRegistry from './GroupRegistry';
 import rehydrate from './rehydrate';
 
-const hasRehydrated = !IS_BROWSER;
+// Ensure we only rehydrate once
+let SHOULD_REHYDRATE = !IS_BROWSER;
 
 class StyleSheet {
   groups: GroupedTag;
 
+  hasRehydrated: boolean;
+
   keys: GroupedKeys;
 
-  constructor(target?: HTMLElement) {
+  constructor(target?: HTMLElement, forceServer?: boolean) {
     // $FlowFixMe
     this.keys = Object.create(null);
 
-    const tag = new DefaultTag(target);
-    this.groups = new RuleGroupTag(tag);
+    const tag = forceServer ? new DefaultTag(target) : new VirtualTag();
+    const groups = (this.groups = new RuleGroupTag(tag));
 
-    if (!hasRehydrated) {
-      this.rehydrate();
+    if (!forceServer && SHOULD_REHYDRATE) {
+      SHOULD_REHYDRATE = false;
+      rehydrate(groups);
     }
-  }
-
-  rehydrate() {
-    rehydrate(this.groups);
   }
 
   register(name: string): number {
