0e2ff9257c35253da26395420706e9ceefd83fc4
Phil Pluckthun <phil@kitten.sh>
2019-01-29 00:50:50 +0000

    Add reset method for streamed SSR and fix HTML output

--- packages/styled-sheet/src/StyleSheet.js
+++ packages/styled-sheet/src/StyleSheet.js
@@ -10,24 +10,48 @@ import rehydrate from './rehydrate';
 // Ensure we only rehydrate once
 let SHOULD_REHYDRATE = !IS_BROWSER;
 
+const makeRuleGroupTag = (target?: HTMLElement, forceServer?: boolean): GroupedTag => {
+  const tag = forceServer ? new DefaultTag(target) : new VirtualTag();
+  return new RuleGroupTag(tag);
+};
+
 class StyleSheet {
   groups: GroupedTag;
-
+  target: void | HTMLElement;
+  forceServer: boolean;
   hasRehydrated: boolean;
-
   keys: GroupedKeys;
 
   constructor(target?: HTMLElement, forceServer?: boolean) {
     // $FlowFixMe
     this.keys = Object.create(null);
+    this.forceServer = !!forceServer;
+    this.target = target;
 
-    const tag = forceServer ? new DefaultTag(target) : new VirtualTag();
-    const groups = (this.groups = new RuleGroupTag(tag));
-
+    const groups = (this.groups = makeRuleGroupTag(target, forceServer));
     if (!forceServer && SHOULD_REHYDRATE) {
       SHOULD_REHYDRATE = false;
       rehydrate(groups);
     }
+  }
+
+  inject(group: number, key: string, rules: string[]) {
+    if (!this.hasKey(group, key)) {
+      this.registerKey(group, key);
+      this.groups.insertRules(group, rules);
+    }
+  }
+
+  remove(group: number) {
+    // $FlowFixMe
+    this.keys[group] = Object.create(null);
+    this.groups.clearGroup(group);
+  }
+
+  // This is just meant for SSR where it's safe to reset
+  // the inner rule group tag to flush known rules
+  reset() {
+    this.groups = makeRuleGroupTag(this.target, this.forceServer);
   }
 
   hasKey(group: number, key: string): boolean {
@@ -45,19 +69,6 @@ class StyleSheet {
     groupKeys[key] = true;
   }
 
-  inject(group: number, key: string, rules: string[]) {
-    if (!this.hasKey(group, key)) {
-      this.registerKey(group, key);
-      this.groups.insertRules(group, rules);
-    }
-  }
-
-  remove(group: number) {
-    // $FlowFixMe
-    this.keys[group] = Object.create(null);
-    this.groups.clearGroup(group);
-  }
-
   toString(): string {
     let css = '';
     GroupRegistry.forEach((name, group) => {
@@ -73,7 +84,8 @@ class StyleSheet {
   }
 
   toHTML(): string {
-    return wrapInStyleTag(this.toString());
+    const css = this.toString();
+    return css !== '' ? wrapInStyleTag(css) : '';
   }
 }
 
