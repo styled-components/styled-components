9254b70c1af1df83f15afb1e94024f6da56cf8f7
Alan Souza <alansouzati@gmail.com>
2019-03-07 09:44:30 -0800

    Removed hocs, utils, and constructors from secret internals export.

--- packages/styled-components/src/secretInternals.js
+++ packages/styled-components/src/secretInternals.js
@@ -1,14 +1,8 @@
 // @flow
-import * as constructors from './constructors';
-import * as hoc from './hoc';
-import * as models from './models';
-import * as utils from './utils';
+import { StyleSheet, StyleSheetContext } from './models';
 
 /* eslint-disable */
 export const __DO_NOT_USE_OR_YOU_WILL_BE_HAUNTED_BY_SPOOKY_GHOSTS = {
-  StyleSheet: models.StyleSheet,
-  constructors,
-  hoc,
-  models,
-  utils,
+  StyleSheet,
+  StyleSheetContext,
 };
