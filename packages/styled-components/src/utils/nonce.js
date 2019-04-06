0e4bf1a13a5dfa1ddfc7581427bab4548b1ecee9
Matthew Costabile <mattcosta7@gmail.com>
2019-04-06 11:47:14 -0400

    fix: ensure nonce can be found and applied

--- packages/styled-components/src/utils/nonce.js
+++ packages/styled-components/src/utils/nonce.js
@@ -1,6 +1,13 @@
 // @flow
-/* eslint-disable camelcase, no-undef */
+/* eslint-disable camelcase, no-undef, no-nested-ternary */
 
 declare var __webpack_nonce__: string;
 
-export default () => (typeof __webpack_nonce__ !== 'undefined' ? __webpack_nonce__ : null);
+export default (global => () => {
+  if (typeof global.__webpack_nonce__ !== 'undefined') {
+    return global.__webpack_nonce__;
+  } else if (typeof __webpack_nonce__ !== 'undefined') {
+    return __webpack_nonce__;
+  }
+  return null;
+})(typeof global !== 'undefined' ? global : typeof window !== 'undefined' ? window : {});
