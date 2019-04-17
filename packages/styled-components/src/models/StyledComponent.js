536737826276ed10ec749092067f744354e1c461
Tobias Lohse <MrLoh@users.noreply.github.com>
2019-04-17 18:08:25 -0500

    add displayName to forwardRef

--- packages/styled-components/src/models/StyledComponent.js
+++ packages/styled-components/src/models/StyledComponent.js
@@ -277,15 +277,16 @@ export default function createStyledComponent(target: Target, options: Object, r
    * forwardRef creates a new interim component, which we'll take advantage of
    * instead of extending ParentComponent to create _another_ interim class
    */
-  const WrappedStyledComponent = React.forwardRef((props, ref) => (
+  const forwardRef = (props, ref) => (
     <ParentComponent {...props} forwardedComponent={WrappedStyledComponent} forwardedRef={ref} />
-  ));
+  );
+  forwardRef.displayName = displayName;
+  const WrappedStyledComponent = React.forwardRef(forwardRef)
 
   // $FlowFixMe
   WrappedStyledComponent.attrs = finalAttrs;
   // $FlowFixMe
   WrappedStyledComponent.componentStyle = componentStyle;
-  WrappedStyledComponent.displayName = displayName;
 
   // $FlowFixMe
   WrappedStyledComponent.foldedComponentIds = isTargetStyledComp
