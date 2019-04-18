716e023b4e564ee778066cf9ce3ff7e58e84f24d
Phil Pluckthun <phil@kitten.sh>
2019-04-18 16:51:59 +0100

    Fix more configs after upgrade

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,3 @@
-const { BABEL_ENV, NODE_ENV } = process.env;
-const modules = BABEL_ENV === 'cjs' || NODE_ENV === 'test' ? 'commonjs' : false;
-
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        loose: true,
-        modules,
-      },
-    ],
-    '@babel/preset-react',
-  ],
-  plugins: [
-    '@babel/plugin-external-helpers',
-    'babel-plugin-preval',
-    ['babel-plugin-transform-react-remove-prop-types', { mode: 'unsafe-wrap' }],
-    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    modules === 'commonjs' && 'add-module-exports',
-  ].filter(Boolean),
-};
+  presets: ['./babel-preset.js']
+}
