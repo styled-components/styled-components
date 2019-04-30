05aa3a6796803fcd516472c9bca9c961d7a62b9a
Phil Pluckthun <phil@kitten.sh>
2019-04-30 13:34:15 +0100

    Initialise every sheet's tags lazily

--- packages/styled-components/src/secretInternals.js
+++ packages/styled-components/src/secretInternals.js
@@ -2,9 +2,9 @@
 /* eslint-disable */
 
 import StyleSheet from './sheet';
-import { getMasterStyleSheet } from './models/StyleSheetManager';
+import { masterSheet } from './models/StyleSheetManager';
 
 export const __DO_NOT_USE_OR_YOU_WILL_BE_HAUNTED_BY_SPOOKY_GHOSTS = {
   StyleSheet,
-  getMasterStyleSheet,
+  masterSheet,
 };
