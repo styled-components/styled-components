d024f02ba1ad49780ba572ecbfd6a48d4f242646
Phil Pluckthun <phil@kitten.sh>
2019-04-30 14:53:28 +0100

    Enable SSR rehydration and add active attr

--- packages/styled-components/src/constants.js
+++ packages/styled-components/src/constants.js
@@ -3,21 +3,15 @@
 declare var SC_DISABLE_SPEEDY: ?boolean;
 declare var __VERSION__: string;
 
-export const SC_ATTR =
-  (typeof process !== 'undefined' && (process.env.REACT_APP_SC_ATTR || process.env.SC_ATTR)) ||
-  'data-styled';
-
+export const SC_ATTR = (typeof process !== 'undefined' && process.env.SC_ATTR) || 'data-styled';
+export const SC_ATTR_ACTIVE = 'active';
 export const SC_VERSION_ATTR = 'data-styled-version';
 export const SC_VERSION = __VERSION__;
-
-export const SC_STREAM_ATTR = 'data-styled-streamed';
 
 export const IS_BROWSER = typeof window !== 'undefined' && 'HTMLElement' in window;
 
 export const DISABLE_SPEEDY =
   (typeof SC_DISABLE_SPEEDY === 'boolean' && SC_DISABLE_SPEEDY) ||
-  (typeof process !== 'undefined' &&
-    (process.env.REACT_APP_SC_DISABLE_SPEEDY || process.env.SC_DISABLE_SPEEDY)) ||
   process.env.NODE_ENV !== 'production';
 
 // Shared empty execution context when generating static styles
