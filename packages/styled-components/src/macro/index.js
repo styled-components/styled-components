0164a8d8122ffe5eaee7894851202231add1eac6
James DiGioia <jamesorodig@gmail.com>
2019-05-29 11:26:46 -0400

    Fix macro with ts canary (#2568)
    * Add test for multiple imports
    
    Check that the imports are split up normally.
    
    * Fix macro usage with TypeScript
    
    Fixes #2378.

--- packages/styled-components/src/macro/index.js
+++ packages/styled-components/src/macro/index.js
@@ -1,26 +1,9 @@
 // @flow
-
-import { createMacro, MacroError } from 'babel-plugin-macros';
+import { createMacro } from 'babel-plugin-macros';
 import babelPlugin from 'babel-plugin-styled-components';
 import { addDefault, addNamed } from '@babel/helper-module-imports';
 
 function styledComponentsMacro({ references, state, babel: { types: t }, config = {} }) {
-  const allowedImports = [
-    'default',
-    'createGlobalStyle',
-    'css',
-    'isStyledComponent',
-    'keyframes',
-    'ServerStyleSheet',
-    'StyleSheetConsumer',
-    'StyleSheetContext',
-    'StyleSheetManager',
-    'ThemeConsumer',
-    'ThemeContext',
-    'ThemeProvider',
-    'withTheme'
-  ];
-
   const program = state.file.path;
 
   // FIRST STEP : replace `styled-components/macro` by `styled-components
@@ -28,14 +11,6 @@ function styledComponentsMacro({ references, state, babel: { types: t }, config 
   // { default: [path, path], css: [path], ... }
   let customImportName;
   Object.keys(references).forEach(refName => {
-    if (!allowedImports.includes(refName)) {
-      throw new MacroError(
-        `Invalid import: ${refName}. You can only import ${allowedImports.join(
-          ', '
-        )} from 'styled-components/macro'.`
-      );
-    }
-
     // generate new identifier
     let id;
     if (refName === 'default') {
