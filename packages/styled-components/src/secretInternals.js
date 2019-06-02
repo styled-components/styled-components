7fdfeb3330ff710266d66557e779ac0e4ec1eeeb
Evan Jacobs <probablyup@gmail.com>
2019-06-02 12:29:28 -0500

    shave off some unnecessary bytes

--- packages/styled-components/src/secretInternals.js
+++ packages/styled-components/src/secretInternals.js
@@ -4,7 +4,7 @@
 import StyleSheet from './sheet';
 import { masterSheet } from './models/StyleSheetManager';
 
-export const __DO_NOT_USE_OR_YOU_WILL_BE_HAUNTED_BY_SPOOKY_GHOSTS = {
+export const __PRIVATE__ = {
   StyleSheet,
   masterSheet,
 };
