38a40d7812189d3f97f0afc0499c75a3fb4b24d3
James K Nelson <james@jamesknelson.com>
2019-06-20 11:21:02 +0900

    fix macro on create-react-app

--- packages/styled-components/src/macro/index.js
+++ packages/styled-components/src/macro/index.js
@@ -2,6 +2,7 @@
 import { createMacro } from 'babel-plugin-macros';
 import babelPlugin from 'babel-plugin-styled-components';
 import { addDefault, addNamed } from '@babel/helper-module-imports';
+import traverse from '@babel/traverse';
 
 function styledComponentsMacro({ references, state, babel: { types: t }, config = {} }) {
   const program = state.file.path;
@@ -29,7 +30,12 @@ function styledComponentsMacro({ references, state, babel: { types: t }, config 
 
   // SECOND STEP : apply babel-plugin-styled-components to the file
   const stateWithOpts = { ...state, opts: config, customImportName };
-  program.traverse(babelPlugin({ types: t }).visitor, stateWithOpts);
+  traverse(
+    program.parent,
+    babelPlugin({ types: t }).visitor,
+    undefined,
+    stateWithOpts,
+  )
 }
 
 const configName = 'styledComponents';
