0c657192328ead7f1f1c35008191fb94f464aeb6
Evan Jacobs <probablyup@gmail.com>
2019-06-22 10:30:23 -0500

    lower the threshold for the "too many classes" warning (#2622)
    If your component is dynamic enough to have 50+ styling permutations
    it's probably time to inline some stuff or the stylesheet will
    be massive.

--- packages/styled-components/src/utils/createWarnTooManyClasses.js
+++ packages/styled-components/src/utils/createWarnTooManyClasses.js
@@ -1,6 +1,6 @@
 // @flow
 
-const LIMIT = 200;
+export const LIMIT = 50;
 
 export default (displayName: string) => {
   let generatedClasses = {};
@@ -13,12 +13,12 @@ export default (displayName: string) => {
         // Unable to find latestRule in test environment.
         /* eslint-disable no-console, prefer-template */
         console.warn(
-          `Over ${LIMIT} classes were generated for component ${displayName}. \n` +
-            'Consider using the attrs method, together with a style object for frequently changed styles.\n' +
+          `Over ${LIMIT} classes were generated for component ${displayName}. This happens when some of the props you use for styling have many potential values and we need to make a new CSS class for each variant. Over time the stylesheet will grow and slow down your app.\n` +
+            'For these particular CSS rules with high dynamicity, consider using the attrs() method together with a style object.\n' +
             'Example:\n' +
-            '  const Component = styled.div.attrs(p => ({\n' +
+            '  const Component = styled.div.attrs(props => ({\n' +
             '    style: {\n' +
-            '      background: p.background,\n' +
+            '      background: props.background,\n' +
             '    },\n' +
             '  }))`width: 100%;`\n\n' +
             '  <Component />'
