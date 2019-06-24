f7c8cceba036b0415fc781aee534a1e3f94c134f
Evan Jacobs <probablyup@gmail.com>
2019-06-24 11:47:20 -0500

    remove annoying, unnecessary lint checks

--- packages/styled-components/src/utils/determineTheme.js
+++ packages/styled-components/src/utils/determineTheme.js
@@ -8,11 +8,8 @@ type Props = {
 export default (props: Props, fallbackTheme: any, defaultProps: any = EMPTY_OBJECT) => {
   // Props should take precedence over ThemeProvider, which should take precedence over
   // defaultProps, but React automatically puts defaultProps on props.
-
-  /* eslint-disable react/prop-types, flowtype-errors/show-errors */
   const isDefaultTheme = defaultProps ? props.theme === defaultProps.theme : false;
   const theme = props.theme && !isDefaultTheme ? props.theme : fallbackTheme || defaultProps.theme;
-  /* eslint-enable */
 
   return theme;
 };
