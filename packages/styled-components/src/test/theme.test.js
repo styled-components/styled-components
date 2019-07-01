f1f62440668af2e9ad9b9376f052cac8416672dd
Evan Jacobs <probablyup@gmail.com>
2019-07-01 09:13:36 -0500

    add lightweight dev warning when theme is consumed but not provided (#2655)
    * add lightweight dev warning when theme is consumed but not provided
    
    alternate fix for #2605
    
    * console.warn -> console.error

--- packages/styled-components/src/test/theme.test.js
+++ packages/styled-components/src/test/theme.test.js
@@ -11,6 +11,7 @@ let styled;
 
 describe('theming', () => {
   beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
     styled = resetStyled();
   });
 
@@ -192,12 +193,6 @@ describe('theming', () => {
     theme = newTheme;
     renderComp();
     expectCSSMatches(`${initialCSS} .c { color:${newTheme.color}; }`);
-  });
-});
-
-describe('theming', () => {
-  beforeEach(() => {
-    styled = resetStyled();
   });
 
   it('should properly render with the same theme from default props on re-render', () => {
@@ -566,5 +561,33 @@ describe('theming', () => {
       );
       expect(mock).toHaveBeenCalledTimes(1);
     }).toThrowErrorMatchingSnapshot();
+  });
+
+  it('should warn when trying to access theme in interpolations without a provided theme', () => {
+    const Comp = styled.div`
+      color: ${props => props.theme.color};
+    `;
+
+    TestRenderer.create(<Comp />);
+    expect(console.error).toHaveBeenCalledTimes(1);
+    expect(console.error.mock.calls[0][0]).toMatchInlineSnapshot(
+      `"Component styled.div (.sc-a) uses \\"props.theme\\" in its styles but no theme was provided via prop or ThemeProvider."`
+    );
+
+    /* eslint-disable dot-notation */
+    const Comp2 = styled.div`
+      color: ${props => props.theme['color']};
+    `;
+    /* eslint-enable dot-notation */
+
+    const wrapper = TestRenderer.create(<Comp2 />);
+    expect(console.error).toHaveBeenCalledTimes(2);
+    expect(console.error.mock.calls[1][0]).toMatchInlineSnapshot(
+      `"Component styled.div (.sc-c) uses \\"props.theme\\" in its styles but no theme was provided via prop or ThemeProvider."`
+    );
+
+    // should only error once
+    wrapper.update(<Comp2>Hi</Comp2>);
+    expect(console.error).toHaveBeenCalledTimes(2);
   });
 });
