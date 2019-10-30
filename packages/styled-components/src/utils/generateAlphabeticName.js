418de556e2fedc236b97eee83996f2a2b3cca0fc
Evan Jacobs <probablyup@gmail.com>
2019-10-30 13:48:16 -0400

    disallow /ad/i in generated class names (#2837)
    * disallow /ad/i in generated class names
    
    fixes #2803
    
    doing a simple replacement for these two characters when they're
    preceded by a case-insensitive "a" character since many adblockers
    use this as a hook
    
    * simpler logic
    
    * changelog entry

--- packages/styled-components/src/utils/generateAlphabeticName.js
+++ packages/styled-components/src/utils/generateAlphabeticName.js
@@ -1,6 +1,8 @@
 // @flow
 /* eslint-disable no-bitwise */
 
+const AD_REPLACER_R = /(a)(d)/gi;
+
 /* This is the "capacity" of our alphabet i.e. 2x26 for all letters plus their capitalised
  * counterparts */
 const charsLength = 52;
@@ -15,9 +17,9 @@ export default function generateAlphabeticName(code: number): string {
   let x;
 
   /* get a char and divide by alphabet-length */
-  for (x = Math.abs(code); x > charsLength; x = x / charsLength | 0) {
+  for (x = Math.abs(code); x > charsLength; x = (x / charsLength) | 0) {
     name = getAlphabeticChar(x % charsLength) + name;
   }
 
-  return getAlphabeticChar(x % charsLength) + name;
+  return (getAlphabeticChar(x % charsLength) + name).replace(AD_REPLACER_R, '$1-$2');
 }
