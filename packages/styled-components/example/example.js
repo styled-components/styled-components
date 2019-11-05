b7b4275da7b1a91202840201262b92e6062aeb8c
Samuli Ulmanen <samuli.ulmanen@wunderdog.fi>
2019-11-05 11:14:28 +0200

    [styled-components] add ThemeProvider to Example

--- packages/styled-components/example/example.js
+++ packages/styled-components/example/example.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { createGlobalStyle, keyframes } from '..';
+import styled, { createGlobalStyle, keyframes, ThemeProvider } from '..';
 
 export default () => {
   const GlobalStyle = createGlobalStyle`
@@ -13,7 +13,7 @@ export default () => {
   const Title = styled.h1`
     font-size: 1.5em;
     text-align: center;
-    color: palevioletred;
+    color: ${props => props.theme.palevioletred};
     animation: ${keyframes`from { opacity: 0; }`} 1s both;
   `;
 
@@ -21,16 +21,18 @@ export default () => {
   // some padding and a papayawhip background
   const Wrapper = styled.section`
     padding: 4em;
-    background: papayawhip;
+    background: ${props => props.theme.papayawhip};
   `;
 
   return class Example extends React.Component {
     render() {
       return (
-        <Wrapper>
-          <GlobalStyle />
-          <Title>Hello World, this is my first styled component!</Title>
-        </Wrapper>
+        <ThemeProvider theme={{palevioletred: '#db7093', papayawhip: '#ffefd5'}}>
+          <Wrapper>
+            <GlobalStyle />
+            <Title>Hello World, this is my first styled component!</Title>
+          </Wrapper>
+        </ThemeProvider>
       );
     }
   };
