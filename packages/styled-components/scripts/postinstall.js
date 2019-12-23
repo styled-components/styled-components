7a6cbc3a55d420285a1a307735da1c5361bc60fd
Toru Kobayashi <koba0004@gmail.com>
2019-12-23 10:36:58 +0900

    use funding field instead of post install script

--- packages/styled-components/scripts/postinstall.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-/* eslint-disable */
-/* Adapted from nodemon's postinstall: https://github.com/remy/nodemon/blob/master/package.json */
-
-var msg =
-  'Use styled-components at work? Consider supporting our development efforts at https://opencollective.com/styled-components';
-
-console.log(msg);
