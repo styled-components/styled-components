88d9dd99c764b8134611dfa35ed8cc04807a1e6f
Evan Jacobs <probablyup@gmail.com>
2019-12-29 22:29:03 -0500

    just use phash

--- packages/styled-components/src/utils/stylis.js
+++ packages/styled-components/src/utils/stylis.js
@@ -3,7 +3,7 @@ import _insertRulePlugin from 'stylis-rule-sheet';
 import { type Stringifier } from '../types';
 import { EMPTY_ARRAY, EMPTY_OBJECT } from './empties';
 import throwStyledError from './error';
-import { hash, phash } from './hash';
+import { phash } from './hash';
 
 const COMMENT_REGEX = /^\s*\/\/.*$/gm;
 
@@ -98,8 +98,8 @@ export default function createStylisInstance({
         throwStyledError(15);
       }
 
-      return !acc ? hash(plugin.name) : phash(acc, plugin.name);
-    }, '')
+      return phash(acc, plugin.name);
+    }, 0)
     .toString();
 
   return stringifyRules;
