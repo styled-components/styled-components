95cb25b4009912ff22fc1cb3871a58f885b2dee8
Keegan Street <keegan.street@rea-group.com>
2020-03-20 09:08:17 +1100

    Update isPlainObject test to support objects without a toString method

--- packages/styled-components/src/utils/isPlainObject.js
+++ packages/styled-components/src/utils/isPlainObject.js
@@ -4,5 +4,5 @@ import { typeOf } from 'react-is';
 export default (x: any): boolean =>
   x !== null &&
   typeof x === 'object' &&
-  x.toString() === '[object Object]' &&
+  (x.toString ? x.toString() : Object.prototype.toString.call(x)) === '[object Object]' &&
   !typeOf(x);
