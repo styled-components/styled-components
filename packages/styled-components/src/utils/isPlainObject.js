04795c1b6809920d16d761e9db5bb2d6fe2ff120
Keegan Street <keegan.street@rea-group.com>
2020-03-19 14:57:19 +1100

    Changed isPlainObject to support objects created in a different context

--- packages/styled-components/src/utils/isPlainObject.js
+++ packages/styled-components/src/utils/isPlainObject.js
@@ -1,2 +1,8 @@
 // @flow
-export default (x: any): boolean => typeof x === 'object' && x.constructor === Object;
+import { typeOf } from 'react-is';
+
+export default (x: any): boolean =>
+  x !== null &&
+  typeof x === 'object' &&
+  x.toString() === '[object Object]' &&
+  !typeOf(x);
