34baad863de94fd821d68d2ab31e2167588bc64d
驳是 <jianchun.wjc@alibaba-inc.com>
2020-08-14 10:33:38 +0800

    Add SC_CLASS_PREFIX which can be configured during bundle time to avoid class name collision issue occured when multiple styled-components co-exist in one page

--- packages/styled-components/src/constants.js
+++ packages/styled-components/src/constants.js
@@ -7,6 +7,12 @@ export const SC_ATTR =
   (typeof process !== 'undefined' && (process.env.REACT_APP_SC_ATTR || process.env.SC_ATTR)) ||
   'data-styled';
 
+// Allowing user to add class prefix to all generated class names to avoid class name collision.
+// This is very handy when it comes to situation where different styled-components might co-exist
+// in different JS bundles.
+export const SC_CLASS_PREFIX =
+  (typeof process !== 'undefined' && process.env.SC_CLASS_PREFIX) || '';
+
 export const SC_ATTR_ACTIVE = 'active';
 export const SC_ATTR_VERSION = 'data-styled-version';
 export const SC_VERSION = __VERSION__;
