34296157e3cf16111c84229a509411ba9480c10a
Evan Jacobs <probablyup@gmail.com>
2020-08-27 11:41:55 -0400

    more compact typedef

--- packages/styled-components/src/types.js
+++ packages/styled-components/src/types.js
@@ -7,31 +7,28 @@ export type Interpolation =
   | ((executionContext: Object) => Interpolation)
   | string
   | ComponentType<*>
-  | Array<Interpolation>;
+  | Interpolation[];
 
-export type RuleSet = Array<Interpolation>;
+export type RuleSet = Interpolation[];
 
-export type Styles = Array<string> | Object | ((executionContext: Object) => Interpolation);
+export type Styles = string[] | Object | ((executionContext: Object) => Interpolation);
 
 export type Target = string | ComponentType<*>;
 
 export type NameGenerator = (hash: number) => string;
 
-export type CSSConstructor = (
-  strings: Array<string>,
-  ...interpolations: Array<Interpolation>
-) => RuleSet;
+export type CSSConstructor = (strings: string[], ...interpolations: Interpolation[]) => RuleSet;
 export type StyleSheet = {
   create: Function,
 };
 
 export type Flattener = (
-  chunks: Array<Interpolation>,
+  chunks: Interpolation[],
   executionContext: ?Object,
   styleSheet: ?Object
-) => Array<Interpolation>;
+) => Interpolation[];
 
 export type Stringifier = {
-  (rules: string, selector: string, prefix: ?string, componentId: ?string): Array<string>,
+  (rules: string, selector: string, prefix: ?string, componentId: ?string): string[],
   hash: string,
 };
