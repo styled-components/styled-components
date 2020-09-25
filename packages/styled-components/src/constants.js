3b5f86cc040fb71975a35ca308007eea65080712
Trevor Lohrbeer <trevor@fastfedora.com>
2020-09-25 22:58:10 +0200

    fix: Allow `DISABLE_SPEEDY` to be set to `false`
    The old initialization of `DISABLE_SPEEDY` meant that the only way it could be set to a falsey value was if `NODE_ENV` was set to `production` and no other setting was set. Setting `SC_DISABLE_SPEEDY` or the environment variables to false would be overridden by `NODE_ENV`.
    
    The new initialization ensures that if one of these options is set, it’s value is respected.
    
    This change should only affect the computed value of `DISABLE_SPEEDY` if one of the settings has the value `false` or `’false’`. Otherwise it should have the same truthy or falsey value.
    
    Since the expected behavior of setting one of the configuration variables to `false` is to have `DISABLE_SPEEDY` set to false, this should be a fix and not a breaking change.

--- packages/styled-components/src/constants.js
+++ packages/styled-components/src/constants.js
@@ -14,11 +14,25 @@ export const SPLITTER = '/*!sc*/\n';
 
 export const IS_BROWSER = typeof window !== 'undefined' && 'HTMLElement' in window;
 
+function hasEnvValue(name) {
+  return typeof process !== 'undefined' && typeof process.env[name] !== 'undefined' &&
+    process.env[name] !== '';
+}
+
+function getEnvValue(name) {
+  return process.env[name] === 'false' ? false : process.env[name];
+}
+
 export const DISABLE_SPEEDY =
-  (typeof SC_DISABLE_SPEEDY === 'boolean' && SC_DISABLE_SPEEDY) ||
-  (typeof process !== 'undefined' &&
-    (process.env.REACT_APP_SC_DISABLE_SPEEDY || process.env.SC_DISABLE_SPEEDY)) ||
-  process.env.NODE_ENV !== 'production';
+  typeof SC_DISABLE_SPEEDY === 'boolean'
+    ? SC_DISABLE_SPEEDY
+    : (hasEnvValue('REACT_APP_SC_DISABLE_SPEEDY')
+      ? getEnvValue('REACT_APP_SC_DISABLE_SPEEDY')
+      : (hasEnvValue('SC_DISABLE_SPEEDY')
+        ? getEnvValue('SC_DISABLE_SPEEDY')
+        : process.env.NODE_ENV !== 'production'
+      )
+    );
 
 // Shared empty execution context when generating static styles
 export const STATIC_EXECUTION_CONTEXT = {};
