d744b382424efd66bf4e7d39b2573f8d21ccbb58
Evan Jacobs <570070+probablyup@users.noreply.github.com>
2021-01-21 19:49:43 -0500

    Update packages/styled-components/src/utils/isPlainObject.js
    Co-authored-by: Jason Miller <developit@users.noreply.github.com>

--- packages/styled-components/src/utils/isPlainObject.js
+++ packages/styled-components/src/utils/isPlainObject.js
@@ -5,4 +5,4 @@ export default (x: any): boolean =>
   typeof x === 'object' &&
   (x.toString ? x.toString() : Object.prototype.toString.call(x)) === '[object Object]' &&
   /* check for reasonable markers that the object isn't an element for react & preact/compat */
-  !(x.$$typeof && ('_owner' in x) || x.__v);
+  !('props' in x && (x.$$typeof || x.constructor === undefined));
