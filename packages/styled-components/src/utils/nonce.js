57a3b1dff92b393bac2abfa6391b3d6ada0f3bd4
Andrija Plavsa <Andrija.Plavsa@maurer-electronics.hr>
2021-02-02 16:05:42 +0100

    Implemented CSP nonce handling like in css-in-js library

--- packages/styled-components/src/utils/nonce.js
+++ packages/styled-components/src/utils/nonce.js
@@ -1,9 +1,18 @@
 // @flow
 /* eslint-disable camelcase, no-undef */
 
+import { IS_BROWSER } from "../constants";
+
 declare var __webpack_nonce__: string;
 
 const getNonce = () => {
+
+  if(IS_BROWSER) {
+    const cspNonceMetaTag = document && document.head && document.head.querySelector("meta[property='csp-nonce']");
+    if(cspNonceMetaTag) {
+      return cspNonceMetaTag && cspNonceMetaTag.textContent;
+    }
+  }
   return typeof __webpack_nonce__ !== 'undefined' ? __webpack_nonce__ : null;
 };
 
