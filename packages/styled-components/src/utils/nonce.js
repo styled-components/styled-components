72f879d77760d1d6fa2c224fb479598bcb65ba24
Andrija Plavsa <andrijaplavsa@gmail.com>
2021-02-23 22:29:37 +0100

    NO_TICKET formatting fix

--- packages/styled-components/src/utils/nonce.js
+++ packages/styled-components/src/utils/nonce.js
@@ -1,15 +1,15 @@
 // @flow
 /* eslint-disable camelcase, no-undef */
 
-import { IS_BROWSER } from "../constants";
+import { IS_BROWSER } from '../constants';
 
 declare var __webpack_nonce__: string;
 
 const getNonce = () => {
-
-  if(IS_BROWSER) {
-    const cspNonceMetaTag = document && document.head && document.head.querySelector("meta[property='csp-nonce']");
-    if(cspNonceMetaTag) {
+  if (IS_BROWSER) {
+    const cspNonceMetaTag =
+      document && document.head && document.head.querySelector("meta[property='csp-nonce']");
+    if (cspNonceMetaTag) {
       // $FlowIgnore[prop-missing]
       return cspNonceMetaTag && cspNonceMetaTag.content;
     }
