d8f7b05c4148fffec7eee2680dc849beddf0b336
Till Hainbach <Till.Hainbach@posteo.de>
2021-03-31 09:20:40 +0200

    fix(ssr): fix 'window' undefined reference error

--- packages/styled-components/src/utils/nonce.js
+++ packages/styled-components/src/utils/nonce.js
@@ -4,7 +4,12 @@
 declare var window: { __webpack_nonce__: string };
 
 const getNonce = () => {
-  return typeof window.__webpack_nonce__ !== 'undefined' ? window.__webpack_nonce__ : null;
+
+  return typeof window !== 'undefined'
+    ? typeof window.__webpack_nonce__ !== 'undefined'
+      ? window.__webpack_nonce__
+      : null
+    : null;
 };
 
 export default getNonce;
