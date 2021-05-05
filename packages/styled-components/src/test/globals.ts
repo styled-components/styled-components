9d82b2d1c2a13df4a6e04ea4eb03a9795dc6db3b
Evan Jacobs <probablyup@gmail.com>
2021-05-05 12:16:45 -0400

    shush eslint

--- packages/styled-components/src/test/globals.ts
+++ packages/styled-components/src/test/globals.ts
@@ -1,6 +1,6 @@
+/* eslint-disable no-underscore-dangle, no-var */
 declare var __SERVER__: boolean;
 declare var __VERSION__: string;
 
-/* eslint-disable no-underscore-dangle */
 global.__SERVER__ = typeof document === 'undefined';
 global.__VERSION__ = 'JEST_MOCK_VERSION';
