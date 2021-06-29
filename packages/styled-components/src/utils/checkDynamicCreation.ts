4dbbcd48173c985bcf36d984d2835c902fdb038e
eps1lon <silbermann.sebastian@gmail.com>
2021-06-29 10:54:31 +0200

    feat: Check dynamic creation in React 18

--- packages/styled-components/src/utils/checkDynamicCreation.ts
+++ packages/styled-components/src/utils/checkDynamicCreation.ts
@@ -11,13 +11,30 @@ export const checkDynamicCreation = (displayName: string, componentId?: string) 
       "You may see this warning because you've called styled inside another component.\n" +
       'To resolve this only create new StyledComponents outside of any render method and function component.';
 
+    // If a hook is called outside of a component:
+    // React 17 and earlier throw an error
+    // React 18 and above use console.error
+
+    const originalConsoleError = console.error
     try {
+      let didNotCallInvalidHook = true
+      console.error = (consoleErrorMessage, ...consoleErrorArgs) => {
+      // The error here is expected, since we're expecting anything that uses `checkDynamicCreation` to
+      // be called outside of a React component.
+        if (invalidHookCallRe.test(consoleErrorMessage)) {
+          didNotCallInvalidHook = false
+          // This shouldn't happen, but resets `warningSeen` if we had this error happen intermittently
+          seen.delete(message);
+        } else {
+          originalConsoleError(consoleErrorMessage, ...consoleErrorArgs);
+        }
+      }
       // We purposefully call `useRef` outside of a component and expect it to throw
       // If it doesn't, then we're inside another component.
       // eslint-disable-next-line react-hooks/rules-of-hooks
       useRef();
 
-      if (!seen.has(message)) {
+      if (didNotCallInvalidHook && !seen.has(message)) {
         // eslint-disable-next-line no-console
         console.warn(message);
         seen.add(message);
@@ -29,6 +46,8 @@ export const checkDynamicCreation = (displayName: string, componentId?: string) 
         // This shouldn't happen, but resets `warningSeen` if we had this error happen intermittently
         seen.delete(message);
       }
+    } finally {
+      console.error = originalConsoleError;
     }
   }
 };
