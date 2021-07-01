97884fbd91e6fa9876e1ac2d6158412b2b4aaa83
Evan Jacobs <probablyup@gmail.com>
2021-07-01 14:28:31 -0400

    remove workspace dependencies

--- scripts/jest/config.main.js
+++ scripts/jest/config.main.js
@@ -4,7 +4,7 @@ const baseConfig = require('./config.base');
 module.exports = Object.assign({}, baseConfig, {
   rootDir: path.join(__dirname, '../../packages/styled-components'),
   roots: ['<rootDir>/src/'],
-  setupFiles: ['raf/polyfill', '<rootDir>/src/test/globals.ts'],
+  setupFiles: ['<rootDir>/src/test/globals.ts'],
   setupFilesAfterEnv: ['<rootDir>/test-utils/setupTestFramework.ts'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['<rootDir>/src/native', '<rootDir>/src/primitives'],
