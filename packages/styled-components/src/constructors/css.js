9fa16b0cec7b139ae6ada1be5a1fa98bedca8bd4
Rogin Farrer <rogin@roginfarrer.com>
2021-07-08 16:59:59 -0400

    Support use of css tagged template inside style objects (#3469)

--- packages/styled-components/src/constructors/css.js
+++ packages/styled-components/src/constructors/css.js
@@ -6,17 +6,29 @@ import isFunction from '../utils/isFunction';
 import flatten from '../utils/flatten';
 import type { Interpolation, RuleSet, Styles } from '../types';
 
+/**
+ * Used when flattening object styles to determine if we should
+ * expand an array of styles.
+ */
+const addTag = arg => {
+  if (Array.isArray(arg)) {
+    // eslint-disable-next-line no-param-reassign
+    arg.isCss = true;
+  }
+  return arg;
+};
+
 export default function css(styles: Styles, ...interpolations: Array<Interpolation>): RuleSet {
   if (isFunction(styles) || isPlainObject(styles)) {
     // $FlowFixMe
-    return flatten(interleave(EMPTY_ARRAY, [styles, ...interpolations]));
+    return addTag(flatten(interleave(EMPTY_ARRAY, [styles, ...interpolations])));
   }
 
-  if(interpolations.length === 0 && styles.length === 1 && typeof styles[0] === "string") {
+  if (interpolations.length === 0 && styles.length === 1 && typeof styles[0] === 'string') {
     // $FlowFixMe
     return styles;
   }
 
   // $FlowFixMe
-  return flatten(interleave(styles, interpolations));
+  return addTag(flatten(interleave(styles, interpolations)));
 }
