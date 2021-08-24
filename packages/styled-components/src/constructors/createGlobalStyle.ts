fde4e44cc9bff71d8e2ed09330a8511d4ef3806f
Phil Pluckthun <phil@kitten.sh>
2021-08-24 10:39:41 +0100

    Fix forced server-side mode not triggering global styles (#3551)
    * Fix forced server-side mode not triggering global styles
    
    Resolve #3506
    
    `createGlobalStyle` components skip the synchronous insertion of styles
    in the browser, which is based on a global variable. However, sheets can
    be forced into server-side mode, which means we need to instead adhere
    to the client-side sheet's settings.
    
    * Prevent server-side layout effect warning

--- packages/styled-components/src/constructors/createGlobalStyle.ts
+++ packages/styled-components/src/constructors/createGlobalStyle.ts
@@ -10,8 +10,6 @@ import determineTheme from '../utils/determineTheme';
 import generateComponentId from '../utils/generateComponentId';
 import css from './css';
 
-declare const __SERVER__: boolean;
-
 export default function createGlobalStyle(
   strings: Styles,
   ...interpolations: Array<Interpolation>
@@ -49,15 +47,17 @@ export default function createGlobalStyle(
       );
     }
 
-    if (__SERVER__) {
+    if (styleSheet.server) {
       renderStyles(instance, props, styleSheet, theme, stylis);
-    } else {
-      // this conditional is fine because it is compiled away for the relevant builds during minification,
-      // resulting in a single unguarded hook call
+    }
+
+    if (!__SERVER__) {
       // eslint-disable-next-line react-hooks/rules-of-hooks
       useLayoutEffect(() => {
-        renderStyles(instance, props, styleSheet, theme, stylis);
-        return () => globalStyle.removeStyles(instance, styleSheet);
+        if (!styleSheet.server) {
+          renderStyles(instance, props, styleSheet, theme, stylis);
+          return () => globalStyle.removeStyles(instance, styleSheet);
+        }
       }, [instance, props, styleSheet, theme, stylis]);
     }
 
