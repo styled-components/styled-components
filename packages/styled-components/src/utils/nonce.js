0b289dbf5a7460c17917cf13184d432a3606a209
Sami Mäki <sami.maki@knowit.fi>
2022-06-14 16:07:45 +0300

    Revert "fix(ssr): fix 'window' undefined reference error"
    This reverts commit d8f7b05c4148fffec7eee2680dc849beddf0b336.

--- packages/styled-components/src/utils/nonce.js
+++ packages/styled-components/src/utils/nonce.js
@@ -4,12 +4,7 @@
 declare var window: { __webpack_nonce__: string };
 
 const getNonce = () => {
-
-  return typeof window !== 'undefined'
-    ? typeof window.__webpack_nonce__ !== 'undefined'
-      ? window.__webpack_nonce__
-      : null
-    : null;
+  return typeof window.__webpack_nonce__ !== 'undefined' ? window.__webpack_nonce__ : null;
 };
 
 export default getNonce;
