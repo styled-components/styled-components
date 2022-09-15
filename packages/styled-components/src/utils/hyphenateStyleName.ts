5bfa5419a2a1e0dec22fa0734951fe626d0cd34f
Aron Griffis <aron@scampersand.com>
2022-09-15 11:21:38 -0400

    fix: css vars mangled by hyphenateStyleName
    Fixes #3810

--- packages/styled-components/src/utils/hyphenateStyleName.ts
+++ packages/styled-components/src/utils/hyphenateStyleName.ts
@@ -2,8 +2,8 @@
  * inlined version of
  * https://github.com/facebook/fbjs/blob/master/packages/fbjs/src/core/hyphenateStyleName.js
  */
-const uppercaseCheck = /([A-Z])/;
-const uppercasePattern = /([A-Z])/g;
+const uppercaseCheck = /[A-Z]/;
+const uppercasePattern = /[A-Z]/g;
 const msPattern = /^ms-/;
 const prefixAndLowerCase = (char: string): string => `-${char.toLowerCase()}`;
 
@@ -21,7 +21,7 @@ const prefixAndLowerCase = (char: string): string => `-${char.toLowerCase()}`;
  * is converted to `-ms-`.
  */
 export default function hyphenateStyleName(string: string) {
-  return uppercaseCheck.test(string)
+  return uppercaseCheck.test(string) && !string.startsWith('--')
     ? string.replace(uppercasePattern, prefixAndLowerCase).replace(msPattern, '-ms-')
     : string;
 }
