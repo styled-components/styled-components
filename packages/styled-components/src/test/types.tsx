ae91c652a03cfa274fc70608f5ec6f2a7bf47c4e
Evan Jacobs <probablyup@gmail.com>
2023-01-29 00:12:06 -0500

    chore: add type test file with failing scenario

---
+++ packages/styled-components/src/test/types.tsx
@@ -0,0 +1,44 @@
+/**
+ * This file is meant for typing-related tests that don't need to go through Jest.
+ */
+import React from 'react';
+import styled from '../index-standalone';
+
+/**
+ * Prop inference when using forwardRef
+ * https://github.com/styled-components/styled-components/issues/3800#issuecomment-1313445733
+ */
+const ComponentBase = React.forwardRef<HTMLDivElement, { prop: 'foo' }>(
+  ({ prop, ...props }, ref) => (
+    <div ref={ref} {...props}>
+      {prop}
+    </div>
+  )
+);
+
+const Component1 = styled(ComponentBase)`
+  border: 1px solid black;
+`;
+
+// prop should resolve to "foo"
+<Component1 prop="foo" />;
+
+/**
+ * interpolated component type incorrectly appended to wrapper
+ * https://github.com/styled-components/styled-components/issues/3800#issuecomment-1296723840
+ */
+const Button = styled.button<{ foo: boolean }>``;
+const Wrapper = styled.div`
+  ${Button} > * {
+    color: red;
+  }
+`;
+
+const Component2 = () => {
+  return (
+    // Wrapper should not be influenced by Button's types
+    <Wrapper>
+      <Button foo>test</Button>
+    </Wrapper>
+  );
+};
