86d8ca3c44ca4d40eab9b6b4833677fc0cdfbcdd
Suhas R <suhas.r@whatfix.com>
2023-03-01 13:48:10 +0530

    fix: prevent crash when process.env is not defined

--- packages/styled-components/src/constants.js
+++ packages/styled-components/src/constants.js
@@ -4,7 +4,7 @@ declare var SC_DISABLE_SPEEDY: ?boolean;
 declare var __VERSION__: string;
 
 export const SC_ATTR: string =
-  (typeof process !== 'undefined' && (process.env.REACT_APP_SC_ATTR || process.env.SC_ATTR)) ||
+  (typeof process !== 'undefined' && typeof process.env !== 'undefined' && (process.env.REACT_APP_SC_ATTR || process.env.SC_ATTR)) ||
   'data-styled';
 
 export const SC_ATTR_ACTIVE = 'active';
@@ -17,11 +17,11 @@ export const IS_BROWSER = typeof window !== 'undefined' && 'HTMLElement' in wind
 export const DISABLE_SPEEDY =
   Boolean(typeof SC_DISABLE_SPEEDY === 'boolean'
     ? SC_DISABLE_SPEEDY
-    : (typeof process !== 'undefined' && typeof process.env.REACT_APP_SC_DISABLE_SPEEDY !== 'undefined' && process.env.REACT_APP_SC_DISABLE_SPEEDY !== ''
+    : (typeof process !== 'undefined' && typeof process.env !== 'undefined' && typeof process.env.REACT_APP_SC_DISABLE_SPEEDY !== 'undefined' && process.env.REACT_APP_SC_DISABLE_SPEEDY !== ''
       ? process.env.REACT_APP_SC_DISABLE_SPEEDY === 'false' ? false : process.env.REACT_APP_SC_DISABLE_SPEEDY
-      : (typeof process !== 'undefined' && typeof process.env.SC_DISABLE_SPEEDY !== 'undefined' && process.env.SC_DISABLE_SPEEDY !== ''
+      : (typeof process !== 'undefined' && typeof process.env !== 'undefined' && typeof process.env.SC_DISABLE_SPEEDY !== 'undefined' && process.env.SC_DISABLE_SPEEDY !== ''
         ? process.env.SC_DISABLE_SPEEDY === 'false' ? false : process.env.SC_DISABLE_SPEEDY
-        : process.env.NODE_ENV !== 'production'
+        : process.env && process.env.NODE_ENV !== 'production'
       )
     ));
 
