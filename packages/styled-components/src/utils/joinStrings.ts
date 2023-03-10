41bd1907a12a8aa32043c3263ba828945caa37e0
Evan Jacobs <probablyup@gmail.com>
2023-03-09 21:52:13 -0500

    refactor: simplify output

--- packages/styled-components/src/utils/joinStrings.ts
+++ packages/styled-components/src/utils/joinStrings.ts
@@ -1,6 +1,6 @@
 /**
  * Convenience function for joining strings to form className chains
  */
-export default function joinStrings(...args: any[]): string {
-  return args.filter(Boolean).join(' ');
+export default function joinStrings(a: string | undefined, b: string): string {
+  return a && b ? `${a} ${b}` : a || b;
 }
