5b296b09461eb8acdae80ee35b1002e2ef546570
Evan Jacobs <probablyup@gmail.com>
2023-06-22 00:31:45 -0400

    chore: massively speed up compilation

--- packages/styled-components/src/constructors/styled.tsx
+++ packages/styled-components/src/constructors/styled.tsx
@@ -12,8 +12,7 @@ const styled = baseStyled as typeof baseStyled & {
 
 // Shorthands for all valid HTML Elements
 domElements.forEach(domElement => {
-  // @ts-expect-error compiler can't handle it
-  styled[domElement] = baseStyled(domElement);
+  styled[domElement] = baseStyled<typeof domElement>(domElement);
 });
 
 export default styled;
