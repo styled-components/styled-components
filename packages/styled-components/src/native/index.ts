f1bec26fe132fb7897b1a0ead59409d4534adb58
Evan Jacobs <ejacobs@aurorasolar.com>
2023-07-24 18:40:56 -0400

    feat(native): ship createThemedNativeFactory, ThemedNativeFactory type

--- packages/styled-components/src/native/index.ts
+++ packages/styled-components/src/native/index.ts
@@ -4,8 +4,13 @@ import css from '../constructors/css';
 import withTheme from '../hoc/withTheme';
 import _InlineStyle from '../models/InlineStyle';
 import _StyledNativeComponent from '../models/StyledNativeComponent';
-import ThemeProvider, { ThemeConsumer, ThemeContext, useTheme } from '../models/ThemeProvider';
-import { NativeTarget } from '../types';
+import ThemeProvider, {
+  DefaultTheme,
+  ThemeConsumer,
+  ThemeContext,
+  useTheme,
+} from '../models/ThemeProvider';
+import { BaseObject, NativeTarget } from '../types';
 import isStyledComponent from '../utils/isStyledComponent';
 
 const reactNative = require('react-native') as Awaited<typeof import('react-native')>;
@@ -14,7 +19,7 @@ const InlineStyle = _InlineStyle(reactNative.StyleSheet);
 const StyledNativeComponent = _StyledNativeComponent(InlineStyle);
 
 const baseStyled = <Target extends NativeTarget>(tag: Target) =>
-  constructWithOptions<'native', Target>(StyledNativeComponent, tag);
+  constructWithOptions<'native', DefaultTheme, Target>(StyledNativeComponent, tag);
 
 /* React native lazy-requires each of these modules for some reason, so let's
  *  assume it's for a good reason and not eagerly load them all */
@@ -54,9 +59,47 @@ type RNComponents = {
     : never;
 };
 
-const styled = baseStyled as typeof baseStyled & {
-  [E in KnownComponents]: Styled<'native', RNComponents[E], React.ComponentProps<RNComponents[E]>>;
+export const styled = baseStyled as typeof baseStyled & {
+  [E in KnownComponents]: Styled<
+    'native',
+    DefaultTheme,
+    RNComponents[E],
+    React.ComponentProps<RNComponents[E]>
+  >;
 };
+
+export type ThemedStyledNativeFactory<Theme extends object = DefaultTheme> = (<
+  Target extends NativeTarget
+>(
+  tag: Target
+) => Styled<
+  'web',
+  Theme,
+  Target,
+  Target extends React.ComponentType<any> ? React.ComponentPropsWithRef<Target> : BaseObject
+>) & {
+  [E in keyof typeof reactNative]: Styled<
+    'native',
+    Theme,
+    RNComponents[E],
+    React.ComponentPropsWithRef<RNComponents[E]>
+  >;
+};
+
+/**
+ * This method is only necessary if there is a desire to provide separate `styled` factories that
+ * are preconfigured for a particular custom theme. Most commonly this will be third-party libraries
+ * composing styled-components that wish to skip the module-augmentation step that is typically
+ * required for providing custom theme data.
+ *
+ * ```tsx
+ * const styled = createThemedFactory<MyCustomTheme>();
+ *
+ * styled.View``
+ * ```
+ */
+export const createThemedNativeFactory = <Theme extends object>() =>
+  styled as ThemedStyledNativeFactory<Theme>;
 
 /* Define a getter for each alias which simply gets the reactNative component
  * and passes it to styled */
@@ -91,4 +134,4 @@ export {
   StyledOptions,
 } from '../types';
 export { css, isStyledComponent, ThemeProvider, ThemeConsumer, ThemeContext, withTheme, useTheme };
-export { styled, styled as default };
+export { styled as default };
