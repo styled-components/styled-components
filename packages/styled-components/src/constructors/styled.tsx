71af2e972178604550a973673d3688fcb9890f5c
Evan Jacobs <probablyup@gmail.com>
2023-09-13 11:04:23 -0400

    chore: suppress apparently corrupted ref type from React

--- packages/styled-components/src/constructors/styled.tsx
+++ packages/styled-components/src/constructors/styled.tsx
@@ -12,6 +12,7 @@ const styled = baseStyled as typeof baseStyled & {
 
 // Shorthands for all valid HTML Elements
 domElements.forEach(domElement => {
+  // @ts-expect-error some react typing bs
   styled[domElement] = baseStyled<typeof domElement>(domElement);
 });
 
