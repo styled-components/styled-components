84ba5b92a6f615528ad99456a355cec77fd2b3f8
Ericsen Tsai <ericsen.tsai@onedegree.hk>
2024-10-04 14:24:01 +0800

    feat: get nonce from process.env or __webpack_nonce__

--- packages/styled-components/src/utils/nonce.ts
+++ packages/styled-components/src/utils/nonce.ts
@@ -1,5 +1,11 @@
 declare let __webpack_nonce__: string;
 
 export default function getNonce() {
-  return typeof __webpack_nonce__ !== 'undefined' ? __webpack_nonce__ : null;
+  let nonce: string | null = null;
+  if (process.env.styled_component_nonce) {
+    nonce = process.env.styled_component_nonce;
+  } else if (typeof __webpack_nonce__ !== 'undefined') {
+    nonce = __webpack_nonce__;
+  }
+  return nonce;
 }
